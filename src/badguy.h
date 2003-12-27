5644b05aa25b65ebe6ce85da7f3264f24f3c4ddb
Tobias Gläßer <tobi.web@gmx.de>
2003-12-27 12:35:59 +0000

    renamed from enemy.h to badguy.h
    SVN-Revision: 51

---
+++ src/badguy.h
@@ -0,0 +1,30 @@
+//
+// C++ Interface: enemy
+//
+// Description:
+//
+//
+// Author: Tobias Glaesser <tobi.web@gmx.de> (C) 2003
+//
+// Copyright: See COPYING file that comes with this distribution
+//
+//
+
+
+/* Types: */
+
+typedef struct bad_guy_type
+  {
+    int alive;
+    int mode;
+    int dying;
+    int timer;
+    int kind;
+    int seen;
+    int dir;
+    int x;
+    int y;
+    int xm;
+    int ym;
+  }
+bad_guy_type;
