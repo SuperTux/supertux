b613901b4b6089d6fc5ee33f9ec76909a42e9f28
Ingo Ruhnke <grumbel@gmx.de>
2004-03-22 13:55:11 +0000

    fixed indention to look a bit nicer
    SVN-Revision: 317

--- src/level.h
+++ src/level.h
@@ -40,35 +40,35 @@ class st_subset
 
 #define LEVEL_NAME_MAX 20
 
-typedef struct st_level /*It is easier to read the sources IMHO, if we don't write something like int a,b,c; */
-  {
-    std::string name;
-    std::string theme;
-    std::string song_title;
-    std::string bkgd_image;
-    unsigned int* tiles[15];
-    int time_left;
-    int bkgd_red;
-    int bkgd_green;
-    int bkgd_blue;
-    int width;
-    float gravity;
-  }
+typedef struct st_level 
+{
+  std::string name;
+  std::string theme;
+  std::string song_title;
+  std::string bkgd_image;
+  unsigned int* tiles[15];
+  int time_left;
+  int bkgd_red;
+  int bkgd_green;
+  int bkgd_blue;
+  int width;
+  float gravity;
+}
 st_level;
 
 extern texture_type img_bkgd, img_bkgd_tile[2][4], img_solid[4], img_brick[2];
 
-void level_default(st_level* plevel);
-int level_load(st_level* plevel, const char * subset, int level);
-void level_parse(st_level* plevel, lisp_object_t* cursor);
-int level_load(st_level* plevel, const char* filename);
-void level_save(st_level* plevel, const char * subset, int level);
-void level_free(st_level* plevel);
-void level_load_gfx(st_level* plevel);
+void level_default  (st_level* plevel);
+int  level_load     (st_level* plevel, const char * subset, int level);
+void level_parse    (st_level* plevel, lisp_object_t* cursor);
+int  level_load     (st_level* plevel, const char* filename);
+void level_save     (st_level* plevel, const char * subset, int level);
+void level_free     (st_level* plevel);
+void level_load_gfx (st_level* plevel);
+void level_change   (st_level* plevel, float x, float y, unsigned char c);
+void level_load_song(st_level* plevel);
 void level_free_gfx();
 void level_load_image(texture_type* ptexture, std::string theme, const char * file, int use_alpha);
-void level_change(st_level* plevel, float x, float y, unsigned char c);
-void level_load_song(st_level* plevel);
 void level_free_song(void);
 
 #endif /*SUPERTUX_LEVEL_H*/
