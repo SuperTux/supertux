14e53e9803643f1a3f7105d8e026422f3d23b70c
Ricardo Cruz <rick2@aeiou.pt>
2004-05-15 14:26:38 +0000

    Ooops, forgot to remove a comment.
    SVN-Revision: 1196

--- src/screen.cpp
+++ src/screen.cpp
@@ -82,8 +82,6 @@ void drawgradient(Color top_clr, Color bot_clr)
   {
 #endif
 
-//void fillrect(float x, float y, float w, float h, int r, int g, int b, int a)
-
     for(float y = 0; y < screen->h; y += 2)
       fillrect(0, (int)y, screen->w, 2,
        (int)(((float)(top_clr.red-bot_clr.red)/(0-screen->h)) * y + top_clr.red),
