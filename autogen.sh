5d2aa42fd02878ec217f2a2eff90d36da7368611
Duong-Khang Nguyen <neoneurone@users.sf.net>
2004-06-07 18:11:11 +0000

    added AUTOMAKE_OPTIONS
    SVN-Revision: 1420

--- autogen.sh
+++ autogen.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
 # we need a minimum of automake 1.6 and automake 1.8 seems to be buggy
-export WANT_AUTOMAKE=1.6
+# this doesn't seem to work, commented out
+# see AUTOMAKE_OPTIONS in Makefile.am
+# export WANT_AUTOMAKE=1.6
 
 aclocal -I m4
 automake --copy --add-missing
