8bcac3a8af4dbb320f3af319a1d1857909b3600a
Duong-Khang Nguyen <neoneurone@users.sf.net>
2004-06-07 19:21:18 +0000

    added missing 'exceptions.h' and 'gettext.h' to Makefile.am
    SVN-Revision: 1421

--- autogen.sh
+++ autogen.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 # we need a minimum of automake 1.6 and automake 1.8 seems to be buggy
-# this doesn't seem to work, commented out
+# this doesn't seem to work well
 # see AUTOMAKE_OPTIONS in Makefile.am
-# export WANT_AUTOMAKE=1.6
+export WANT_AUTOMAKE=1.6
 
 aclocal -I m4
 automake --copy --add-missing
