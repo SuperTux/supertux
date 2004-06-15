9a756d020e12c792adbd6bb3970ce5a6829e35de
Ingo Ruhnke <grumbel@gmx.de>
2004-06-15 22:56:42 +0000

    - moved levelsubset into an own file
    SVN-Revision: 1501

---
+++ src/level_subset.h
@@ -0,0 +1,57 @@
+//  $Id$
+// 
+//  SuperTux
+//  Copyright (C) 2004 SuperTux Development Team, see AUTHORS for details
+//
+//  This program is free software; you can redistribute it and/or
+//  modify it under the terms of the GNU General Public License
+//  as published by the Free Software Foundation; either version 2
+//  of the License, or (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+// 
+//  You should have received a copy of the GNU General Public License
+//  along with this program; if not, write to the Free Software
+//  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
+//  02111-1307, USA.
+
+#ifndef SUPERTUX_LEVEL_SUBSET_H
+#define SUPERTUX_LEVEL_SUBSET_H
+
+#include <string>
+#include "lispreader.h"
+
+class Surface;
+
+/** This type holds meta-information about a level-subset. 
+    It could be extended to handle manipulation of subsets. */
+class LevelSubset
+{
+public:
+  LevelSubset();
+  ~LevelSubset();
+
+  static void create(const std::string& subset_name);
+  void load(const char* subset);
+  void save();
+
+  std::string get_level_filename(unsigned int i);
+
+  std::string name;
+  std::string title;
+  std::string description;
+  Surface* image;
+  int levels;
+ 
+private:
+  void parse(lisp_object_t* cursor);
+};
+
+#endif
+
+/* Local Variables: */
+/* mode:c++ */
+/* End: */
