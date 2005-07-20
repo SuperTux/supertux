d9f6263a08f49c6f4338185a6e42110099a3172c
Marek Moeckel <wansti@gmx.de>
2005-07-20 14:49:01 +0000

    added Mr Fluffy badguy I want him for a secret (he's going to appear only once in the whole game), so please don't mention him anywhere or put him in the editor!
    SVN-Revision: 2739

---
+++ src/badguy/fluffy.cpp
@@ -0,0 +1,99 @@
+//  $Id: Fluffy.cpp 2642 2005-06-26 13:38:53Z matzebraun $
+// 
+//  SuperTux
+//  Copyright (C) 2005 Matthias Braun <matze@braunis.de>
+//
+//  This program is free software; you can redistribute it and/or
+//  modify it under the terms of the GNU General Public License
+//  as published by the Free Software Foundation; either version 2
+//  of the License, or (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+// 
+//  You should have received a copy of the GNU General Public License
+//  along with this program; if not, write to the Free Software
+//  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
+//  02111-1307, USA.
+
+#include <config.h>
+
+#include "fluffy.hpp"
+
+static const float WALKSPEED = 80;
+
+Fluffy::Fluffy(const lisp::Lisp& reader)
+{
+  reader.get("x", start_position.x);
+  reader.get("y", start_position.y);
+  bbox.set_size(31.8, 31.8);
+  sprite = sprite_manager->create("fluffy");
+  set_direction = false;
+}
+
+Fluffy::Fluffy(float pos_x, float pos_y, Direction d)
+{
+  start_position.x = pos_x;
+  start_position.y = pos_y;
+  bbox.set_size(31.8, 31.8);
+  sprite = sprite_manager->create("fluffy");
+  set_direction = true;
+  initial_direction = d;
+}
+
+void
+Fluffy::write(lisp::Writer& writer)
+{
+  writer.start_list("fluffy");
+
+  writer.write_float("x", start_position.x);
+  writer.write_float("y", start_position.y);
+
+  writer.end_list("fluffy");
+}
+
+void
+Fluffy::activate()
+{
+  if (set_direction) {dir = initial_direction;}
+  physic.set_velocity_x(dir == LEFT ? -WALKSPEED : WALKSPEED);
+  sprite->set_action(dir == LEFT ? "left" : "right");
+}
+
+bool
+Fluffy::collision_squished(Player& player)
+{
+  sprite->set_action(dir == LEFT ? "squished-left" : "squished-right");
+  kill_squished(player);
+  return true;
+}
+
+HitResponse
+Fluffy::collision_solid(GameObject& , const CollisionHit& hit)
+{
+  if(fabsf(hit.normal.y) > .5) { // hit floor or roof?
+    physic.set_velocity_y(0);
+  } else { // hit right or left
+    dir = dir == LEFT ? RIGHT : LEFT;
+    sprite->set_action(dir == LEFT ? "left" : "right");
+    physic.set_velocity_x(-physic.get_velocity_x());
+  }
+
+  return CONTINUE;
+}
+
+HitResponse
+Fluffy::collision_badguy(BadGuy& , const CollisionHit& hit)
+{
+  if(fabsf(hit.normal.x) > .8) { // left or right hit
+    dir = dir == LEFT ? RIGHT : LEFT;
+    sprite->set_action(dir == LEFT ? "left" : "right");
+    physic.set_velocity_x(-physic.get_velocity_x());       
+  }
+
+  return CONTINUE;
+}
+
+IMPLEMENT_FACTORY(Fluffy, "fluffy")
