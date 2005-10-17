32c40729adce1876b037e7ee9161e96d8cd42dcf
Matthias Braun <matze@braunis.de>
2005-10-17 09:29:12 +0000

    remove glu dependency again, since it seems some people have trouble compiling it...
    SVN-Revision: 2858

--- src/video/glutil.hpp
+++ src/video/glutil.hpp
@@ -22,7 +22,6 @@
 #include <sstream>
 #include <stdexcept>
 #include <GL/gl.h>
-#include <GL/glu.h>
 
 static inline void assert_gl(const char* message)
 {
@@ -31,7 +30,36 @@ static inline void assert_gl(const char* message)
   if(error != GL_NO_ERROR) {
     std::ostringstream msg;
     msg << "OpenGLError while '" << message << "': ";
-        //<< gluErrorString(error);
+    switch(error) {
+      case GL_INVALID_ENUM:
+        msg << "INVALID_ENUM: An unacceptable value is specified for an "
+               "enumerated argument.";
+        break;
+      case GL_INVALID_VALUE:
+        msg << "INVALID_VALUE: A numeric argument is out of range.";
+        break;
+      case GL_INVALID_OPERATION:
+        msg << "INVALID_OPERATION: The specified operation is not allowed "
+               "in the current state.";
+        break;
+      case GL_STACK_OVERFLOW:
+        msg << "STACK_OVERFLOW: This command would cause a stack overflow.";
+        break;
+      case GL_STACK_UNDERFLOW:
+        msg << "STACK_UNDERFLOW: This command would cause a stack underflow.";
+        break;
+      case GL_OUT_OF_MEMORY:
+        msg << "OUT_OF_MEMORY: There is not enough memory left to execute the "
+               "command.";
+        break;
+      case GL_TABLE_TOO_LARGE:
+        msg << "TABLE_TOO_LARGE: The specified table exceeds the "
+               "implementation's maximum supported table size.";
+        break;
+      default:
+        msg << "Unknown error (code " << error << ")";
+    }
+        
     throw std::runtime_error(msg.str());
   }
 #endif
