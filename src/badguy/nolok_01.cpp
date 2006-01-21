0843beea2772c3070f4694f2d8be2f796ccf3c1c
Matthias Braun <matze@braunis.de>
2006-01-21 12:06:18 +0000

    separate nolok sprite
    SVN-Revision: 3010

--- src/badguy/nolok_01.cpp
+++ src/badguy/nolok_01.cpp
@@ -17,7 +17,6 @@
 //  along with this program; if not, write to the Free Software
 //  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
 //  02111-1307, USA.
-
 #include <config.h>
 
 #include "nolok_01.hpp"
@@ -38,7 +37,7 @@ Nolok_01::Nolok_01(const lisp::Lisp& reader)
   reader.get("x", start_position.x);
   reader.get("y", start_position.y);
   bbox.set_size(31.8, 63.8);
-  sprite = sprite_manager->create("nolok");
+  sprite = sprite_manager->create("images/creatures/nolok/nolok.sprite");
   countMe = false;
 }
 
@@ -47,7 +46,7 @@ Nolok_01::Nolok_01(float pos_x, float pos_y)
   start_position.x = pos_x;
   start_position.y = pos_y;
   bbox.set_size(31.8, 63.8);
-  sprite = sprite_manager->create("nolok");
+  sprite = sprite_manager->create("images/creatures/nolok/nolok.sprite");
   countMe = false;
 }
 
