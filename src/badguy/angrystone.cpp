01549e7cabf36e3e0d0a53fd5d318d7a69cbf1b5
Stephen Groundwater <slacker@gluebox.com>
2006-03-03 12:21:35 +0000

    adding Christoph's badguy code for Grumbel's AngryStone
    SVN-Revision: 3064

---
+++ src/badguy/angrystone.cpp
@@ -0,0 +1,172 @@
+//  $Id: angrystone.cpp 2979 2006-01-10 00:00:04Z matzebraun $
+// 
+//  SuperTux
+//  Copyright (C) 2005 Matthias Braun <matze@braunis.de>
+//
+//  This program is free software; you can redistribute it and/or
+//  modify it under the terms of the GNU General Public License
+//  as published by the Free Software Foundation; either version 2
+//  of the License, or (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+// 
+//  You should have received a copy of the GNU General Public License
+//  along with this program; if not, write to the Free Software
+//  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
+//  02111-1307, USA.
+
+#include <config.h>
+
+#include "angrystone.hpp"
+
+static const float SPEED = 240;
+
+static const float CHARGE_TIME = .5;
+static const float ATTACK_TIME = 1;
+static const float RECOVER_TIME = .5;
+
+AngryStone::AngryStone(const lisp::Lisp& reader)
+{
+  reader.get("x", start_position.x);
+  reader.get("y", start_position.y);
+  bbox.set_size(87.8, 87.8); // sprite is (88px, 88px)
+  sprite = sprite_manager->create("angrystone");
+  state = IDLE;
+}
+
+void
+AngryStone::write(lisp::Writer& writer)
+{
+  writer.start_list("angrystone");
+
+  writer.write_float("x", start_position.x);
+  writer.write_float("y", start_position.y);
+
+  writer.end_list("angrystone");
+}
+
+void
+AngryStone::activate()
+{
+  physic.set_velocity_x(0);
+  physic.set_velocity_y(0);
+  physic.enable_gravity(true);
+  sprite->set_action("idle");
+}
+
+HitResponse
+AngryStone::collision_solid(GameObject& , const CollisionHit& hit)
+{
+  if ((state == ATTACKING) && (hit.normal.x == -attackDirection.x) && (hit.normal.y == attackDirection.y)) {
+    state = IDLE;
+    sprite->set_action("idle");
+    physic.set_velocity_x(0);
+    physic.set_velocity_y(0);
+    physic.enable_gravity(true);
+    oldWallDirection.x = attackDirection.x;
+    oldWallDirection.y = attackDirection.y;
+  }
+
+  return CONTINUE;
+}
+
+HitResponse
+AngryStone::collision_badguy(BadGuy& badguy, const CollisionHit& hit)
+{
+  if (state == ATTACKING) {
+    badguy.kill_fall();
+    return FORCE_MOVE;
+  }
+
+  return FORCE_MOVE;
+}
+
+void 
+AngryStone::active_update(float elapsed_time) {
+  BadGuy::active_update(elapsed_time);
+
+  if (state == IDLE) {
+    MovingObject* player = Sector::current()->player;
+    MovingObject* badguy = this;
+    const Vector playerPos = player->get_pos();
+    const Vector badguyPos = badguy->get_pos();
+    float dx = (playerPos.x - badguyPos.x);
+    float dy = (playerPos.y - badguyPos.y);
+
+    float playerHeight = player->get_bbox().p2.y - player->get_bbox().p1.y;
+    float badguyHeight = badguy->get_bbox().p2.y - badguy->get_bbox().p1.y;
+
+    float playerWidth = player->get_bbox().p2.x - player->get_bbox().p1.x;
+    float badguyWidth = badguy->get_bbox().p2.x - badguy->get_bbox().p1.x;
+
+    if ((dx > -playerWidth) && (dx < badguyWidth)) {
+      if (dy > 0) {
+        attackDirection.x = 0;
+        attackDirection.y = -1;
+      } else {
+        attackDirection.x = 0;
+        attackDirection.y = 1;
+      }
+      if ((attackDirection.x != oldWallDirection.x) || (attackDirection.y != oldWallDirection.y)) {
+        sprite->set_action("charging");
+        timer.start(CHARGE_TIME);
+        state = CHARGING;
+      }
+    } else
+    if ((dy > -playerHeight) && (dy < badguyHeight)) {
+      if (dx > 0) {
+        attackDirection.x = 1;
+        attackDirection.y = 0;
+      } else {
+        attackDirection.x = -1;
+        attackDirection.y = 0;
+      }
+      if ((attackDirection.x != oldWallDirection.x) || (attackDirection.y != oldWallDirection.y)) {
+        sprite->set_action("charging");
+        timer.start(CHARGE_TIME);
+        state = CHARGING;
+      }
+    }
+
+  }
+
+  if (state == CHARGING) {
+    if (timer.check()) {
+      sprite->set_action("attacking");
+      timer.start(ATTACK_TIME);
+      state = ATTACKING;
+      physic.enable_gravity(false);
+      physic.set_velocity_x(SPEED * attackDirection.x);
+      physic.set_velocity_y(SPEED * attackDirection.y);
+      oldWallDirection.x = 0;
+      oldWallDirection.y = 0;
+    }
+  }
+
+  if (state == ATTACKING) {
+    if (timer.check()) {
+      timer.start(RECOVER_TIME);
+      state = RECOVERING;
+      sprite->set_action("idle");
+      physic.enable_gravity(true);
+      physic.set_velocity_x(0);
+      physic.set_velocity_y(0);
+    }
+  }
+
+  if (state == RECOVERING) {
+    if (timer.check()) {
+      state = IDLE;
+      sprite->set_action("idle");
+      physic.enable_gravity(true);
+      physic.set_velocity_x(0);
+      physic.set_velocity_y(0);
+    }
+  }
+
+}
+
+IMPLEMENT_FACTORY(AngryStone, "angrystone")
---
+++ src/badguy/angrystone.hpp
@@ -0,0 +1,53 @@
+//  $Id: angrystone.hpp 2642 2005-06-26 13:38:53Z matzebraun $
+// 
+//  SuperTux
+//  Copyright (C) 2005 Matthias Braun <matze@braunis.de>
+//
+//  This program is free software; you can redistribute it and/or
+//  modify it under the terms of the GNU General Public License
+//  as published by the Free Software Foundation; either version 2
+//  of the License, or (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+// 
+//  You should have received a copy of the GNU General Public License
+//  along with this program; if not, write to the Free Software
+//  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
+//  02111-1307, USA.
+
+#ifndef __ANGRYSTONE_H__
+#define __ANGRYSTONE_H__
+
+#include "badguy.hpp"
+
+class AngryStone : public BadGuy
+{
+public:
+  AngryStone(const lisp::Lisp& reader);
+
+  void activate();
+  void write(lisp::Writer& writer);
+  HitResponse collision_solid(GameObject& other, const CollisionHit& hit);
+  HitResponse collision_badguy(BadGuy& badguy, const CollisionHit& hit);
+  void active_update(float elapsed_time);
+
+protected:
+  Vector attackDirection;  /**< 1-normalized vector of current attack direction */
+  Vector oldWallDirection; /**< if wall was hit during last attack: 1-normalized vector of last attack direction, (0,0) otherwise */
+
+  Timer timer;
+
+  enum AngryStoneState {
+    IDLE,
+    CHARGING,
+    ATTACKING,
+    RECOVERING
+  };
+  AngryStoneState state;
+  
+};
+
+#endif
