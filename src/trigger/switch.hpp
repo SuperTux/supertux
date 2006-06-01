df0ba6e934e68e8274083bf04888ecadb81ecff1
Christoph Sommer <mail@christoph-sommer.de>
2006-06-01 23:09:26 +0000

    #define fix
    SVN-Revision: 3621

--- src/trigger/switch.hpp
+++ src/trigger/switch.hpp
@@ -17,8 +17,8 @@
 //  along with this program; if not, write to the Free Software
 //  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
-#ifndef SUPERTUX_DOOR_H
-#define SUPERTUX_DOOR_H
+#ifndef SUPERTUX_SWITCH_H
+#define SUPERTUX_SWITCH_H
 
 #include <string>
 
