173b5b4d42c3ad52d1563e6a631390b3390dadbe
Christoph Sommer <mail@christoph-sommer.de>
2006-06-01 22:54:36 +0000

    New trigger object "Switch" using public domain images from "mmiikkee12"
    SVN-Revision: 3619

---
+++ src/trigger/switch.cpp
@@ -0,0 +1,113 @@
+//  $Id$
+//
+//  SuperTux - Switch Trigger
+//  Copyright (C) 2006 Christoph Sommer <christoph.sommer@2006.expires.deltadevelopment.de>
+//
+//  This program is free software; you can redistribute it and/or
+//  modify it under the terms of the GNU General Public License
+//  as published by the Free Software Foundation; either version 2
+//  of the License, or (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program; if not, write to the Free Software
+//  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+
+#include <config.h>
+
+#include "switch.hpp"
+#include "object_factory.hpp"
+#include "sprite/sprite.hpp"
+#include "sprite/sprite_manager.hpp"
+#include "sector.hpp"
+
+Switch::Switch(const lisp::Lisp& reader)
+	: state(OFF)
+{
+  if (!reader.get("x", bbox.p1.x)) throw std::runtime_error("no x position set");
+  if (!reader.get("y", bbox.p1.y)) throw std::runtime_error("no y position set");
+  if (!reader.get("sprite", sprite_name)) throw std::runtime_error("no sprite name set");
+  sprite = sprite_manager->create(sprite_name);
+  bbox.set_size(sprite->get_current_hitbox_width(), sprite->get_current_hitbox_height());
+
+  if (!reader.get("script", script)) throw std::runtime_error("no script set");
+}
+
+Switch::~Switch()
+{
+  delete sprite;
+}
+
+void
+Switch::write(lisp::Writer& writer)
+{
+  writer.start_list("switch");
+  writer.write_float("x", bbox.p1.x);
+  writer.write_float("y", bbox.p1.y);
+  writer.write_string("sprite", sprite_name);
+  writer.write_string("script", script);
+  writer.end_list("switch");
+}
+
+void
+Switch::update(float )
+{
+  switch (state) {
+    case OFF:
+      break;
+    case TURN_ON:
+      if(sprite->animation_done()) {
+	std::istringstream stream(script);
+	Sector::current()->run_script(stream, "Switch");
+
+	sprite->set_action("on", 1);
+	state = ON;
+      }
+      break;
+    case ON:
+      if(sprite->animation_done()) {
+	sprite->set_action("turnoff", 1);
+	state = TURN_OFF;
+      }
+      break;
+    case TURN_OFF:
+      if(sprite->animation_done()) {
+	sprite->set_action("off");
+	state = OFF;
+      }
+      break;
+  }
+}
+
+void
+Switch::draw(DrawingContext& context)
+{
+  sprite->draw(context, bbox.p1, LAYER_TILES);
+}
+
+void
+Switch::event(Player& , EventType type)
+{
+  if(type != EVENT_ACTIVATE) return;
+
+  switch (state) {
+    case OFF:
+	sprite->set_action("turnon", 1);
+	state = TURN_ON;
+      break;
+    case TURN_ON:
+      break;
+    case ON:
+      break;
+    case TURN_OFF:
+      break;
+  }
+
+}
+
+IMPLEMENT_FACTORY(Switch, "switch");
+
---
+++ src/trigger/switch.hpp
@@ -0,0 +1,60 @@
+//  $Id$
+//
+//  SuperTux - Switch Trigger
+//  Copyright (C) 2006 Christoph Sommer <christoph.sommer@2006.expires.deltadevelopment.de>
+//
+//  This program is free software; you can redistribute it and/or
+//  modify it under the terms of the GNU General Public License
+//  as published by the Free Software Foundation; either version 2
+//  of the License, or (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program; if not, write to the Free Software
+//  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+
+#ifndef SUPERTUX_DOOR_H
+#define SUPERTUX_DOOR_H
+
+#include <string>
+
+#include "trigger_base.hpp"
+#include "serializable.hpp"
+#include "lisp/lisp.hpp"
+#include "lisp/writer.hpp"
+#include "video/drawing_context.hpp"
+#include "sprite/sprite.hpp"
+
+class Switch : public TriggerBase, public Serializable
+{
+public:
+  Switch(const lisp::Lisp& reader);
+  virtual ~Switch();
+
+  virtual void write(lisp::Writer& writer);
+  
+  virtual void update(float elapsed_time);
+  virtual void draw(DrawingContext& context);
+  virtual void event(Player& player, EventType type);
+
+private:
+  enum SwitchState {
+    OFF,
+    TURN_ON,
+    ON,
+    TURN_OFF
+  };
+
+  std::string sprite_name;
+  Sprite* sprite;
+  std::string script;
+  
+  SwitchState state;
+
+};
+
+#endif
