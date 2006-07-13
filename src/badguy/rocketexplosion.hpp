a11c10918be7bef5a3a6fc30e009fda719ca221f
Ingo Ruhnke <grumbel@gmx.de>
2006-07-13 22:05:24 +0000

    - fixed include guarts
    SVN-Revision: 3999

--- src/badguy/rocketexplosion.hpp
+++ src/badguy/rocketexplosion.hpp
@@ -17,8 +17,8 @@
 //  along with this program; if not, write to the Free Software
 //  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
-#ifndef __BOMB_H__
-#define __BOMB_H__
+#ifndef __ROCKETEXPLOSION_H__
+#define __ROCKETEXPLOSION_H__
 
 #include "badguy.hpp"
 
