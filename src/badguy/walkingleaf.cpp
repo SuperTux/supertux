2aa5d733681fdb1559ca9801c7b69e72a36dd335
Wolfgang Becker <uafr@gmx.de>
2006-08-13 14:00:43 +0000

    Walking Leaf, a badguy easy to be killed that does not jump off his branch or ledge.
    SVN-Revision: 4181

---
+++ src/badguy/walkingleaf.cpp
@@ -0,0 +1,48 @@
+//  $Id$
+//
+//  SuperTux - Walking Leaf
+//  Copyright (C) 2006 Wolfgang Becker <uafr@gmx.de>
+//
+//  This program is free software; you can redistribute it and/or
+//  modify it under the terms of the GNU General Public License
+//  as published by the Free Software Foundation; either version 2
+//  of the License, or (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program; if not, write to the Free Software
+//  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+
+#include <config.h>
+
+#include "walkingleaf.hpp"
+#include "random_generator.hpp"
+#include "object/sprite_particle.hpp"
+
+WalkingLeaf::WalkingLeaf(const lisp::Lisp& reader)
+	: WalkingBadguy(reader, "images/creatures/walkingleaf/walkingleaf.sprite", "left", "right")
+{
+  walk_speed = 60;
+  max_drop_height = 0;
+}
+
+WalkingLeaf::WalkingLeaf(const Vector& pos, Direction d)
+	: WalkingBadguy(pos, d, "images/creatures/walkingleaf/walkingleaf.sprite", "left", "right")
+{
+  walk_speed = 60;
+  max_drop_height = 0;
+}
+
+bool
+WalkingLeaf::collision_squished(Player& player)
+{
+  sprite->set_action(dir == LEFT ? "squished-left" : "squished-right");
+  kill_squished(player);
+  return true;
+}
+
+IMPLEMENT_FACTORY(WalkingLeaf, "walkingleaf")
