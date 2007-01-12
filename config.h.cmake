44e69c96435ae9e30bd7be23e272dc7b217b78a9
Christoph Sommer <mail@christoph-sommer.de>
2007-01-12 17:57:06 +0000

    Basic configure functionality for cmake
    SVN-Revision: 4552

---
+++ config.h.cmake
@@ -0,0 +1,23 @@
+#define PACKAGE_NAME "supertux"
+#define PACKAGE_VERSION "0.3.0"
+
+#cmakedefine DEBUG
+#cmakedefine ENABLE_SQDBG
+
+#cmakedefine ENABLE_BINRELOC
+#ifdef ENABLE_BINRELOC
+#define APPDATADIR ${APPDATADIR}
+#endif
+
+#define SIZEOF_VOID_P ${SIZEOF_VOID_P}
+#if SIZEOF_VOID_P == 8
+#define _SQ64
+#endif
+
+#cmakedefine HAVE_ICONV_CONST
+#ifdef HAVE_ICONV_CONST
+#define ICONV_CONST const
+#else
+#define ICONV_CONST
+#endif
+
