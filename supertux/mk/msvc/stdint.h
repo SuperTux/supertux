431cd7ea828d16f2d6a5b42fd2bc8560c5f5a468
Tim Goya <tuxdev103@gmail.com>
2007-12-16 23:09:02 +0000

    argh, clean out copy
    SVN-Revision: 5205

--- supertux/mk/msvc/stdint.h
+++
@@ -1,7 +0,0 @@
-#ifndef _STDINT_H
-#define _STDINT_H
-
-// SDL config declares most stdint.h stuff...
-#include <sdl_config_win32.h>
-
-#endif
--- supertux/tools/normalize-po-files.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-find -iname '*.po' | xargs -I xx msgmerge --backup=off --no-fuzzy-matching --no-wrap -U xx messages.pot
-
