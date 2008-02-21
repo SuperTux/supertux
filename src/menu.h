a3b0e05564a44bfdc4d759a9441375f2fac0e30a
Ingo Ruhnke <grumbel@gmx.de>
2008-02-21 17:02:31 +0000

    - fixed syntax error that causes newer gcc to fail
    SVN-Revision: 5332

--- src/menu.h
+++ src/menu.h
@@ -207,7 +207,7 @@ public:
 
   bool isToggled(int id);
 
-  void Menu::get_controlfield_key_into_input(MenuItem *item);
+  void get_controlfield_key_into_input(MenuItem *item);
 
   void draw   ();
   void draw_item(int index, int menu_width, int menu_height);
