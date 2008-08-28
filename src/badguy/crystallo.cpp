1a494bee8b3089a2e032488897ef6bac575bb32b
Wolfgang Becker <uafr@gmx.de>
2008-08-28 21:13:03 +0000

    Crystallo, a basic badguy who patrols around a fixed position.
    SVN-Revision: 5754

---
+++ src/badguy/crystallo.cpp
@@ -0,0 +1,65 @@
+//  $Id$
+//
+//  SuperTux - Crystallo
+//  Copyright (C) 2008 Wolfgang Becker <uafr@gmx.de>
+//
+//  This program is free software; you can redistribute it and/or
+//  modify it under the terms of the GNU General Public License
+//  as published by the Free Software Foundation; either version 2
+//  of the License, or (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program; if not, write to the Free Software
+//  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+
+#include <config.h>
+
+#include "crystallo.hpp"
+
+Crystallo::Crystallo(const lisp::Lisp& reader)
+  : WalkingBadguy(reader, "images/creatures/crystallo/crystallo.sprite", "left", "right")
+{
+  walk_speed = 80;
+  max_drop_height = 16;
+  radius = 100;
+  reader.get("radius", radius);
+}
+
+Crystallo::Crystallo(const Vector& pos, Direction d)
+  : WalkingBadguy(pos, d, "images/creatures/crystallo/crystallo.sprite", "left", "right")
+{
+  walk_speed = 80;
+  max_drop_height = 16;
+  radius = 100;
+}
+
+void
+Crystallo::active_update(float elapsed_time)
+{
+  if(get_pos().x > (start_position.x + radius)){
+    if(dir != LEFT){
+      turn_around();
+    }
+  }
+  if( get_pos().x < (start_position.x - radius)){
+    if(dir != RIGHT){
+      turn_around();
+    }
+  }
+  BadGuy::active_update(elapsed_time);
+}
+
+bool
+Crystallo::collision_squished(GameObject& object)
+{
+  sprite->set_action(dir == LEFT ? "shattered-left" : "shattered-right");
+  kill_squished(object);
+  return true;
+}
+
+IMPLEMENT_FACTORY(Crystallo, "crystallo")
---
+++ src/badguy/crystallo.hpp
@@ -0,0 +1,45 @@
+//  $Id$
+//
+//  SuperTux - Crystallo
+//  Copyright (C) 2008 Wolfgang Becker <uafr@gmx.de>
+//
+//  This program is free software; you can redistribute it and/or
+//  modify it under the terms of the GNU General Public License
+//  as published by the Free Software Foundation; either version 2
+//  of the License, or (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program; if not, write to the Free Software
+//  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+
+#ifndef __CRYSTALLO_H__
+#define __CRSYTALLO_H__
+
+#include "walking_badguy.hpp"
+
+/*
+ * Basic badguy, patrols around a fixed position.
+ */
+class Crystallo : public WalkingBadguy
+{
+public:
+  Crystallo(const lisp::Lisp& reader);
+  Crystallo(const Vector& pos, Direction d);
+
+  virtual Crystallo* clone() const { return new Crystallo(*this); }
+
+  void active_update(float elapsed_time);
+
+protected:
+  bool collision_squished(GameObject& object);
+
+private:
+  float radius;
+};
+
+#endif
