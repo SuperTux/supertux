2dfeff6e47786b3cc030eba72136755d8137845a
Arvid Norlander <anmaster@berlios.de>
2008-09-22 19:10:40 +0000

    Fix curl include paths that were messed up in 5778. The other part of the fix a C++ (not C/C#) developer needs to verify. Thanks to MMlosh for pointing it out.
    SVN-Revision: 5780

--- src/addon/addon_manager.cpp
+++ src/addon/addon_manager.cpp
@@ -38,9 +38,9 @@
 #include "physfs/physfs_stream.hpp"
 
 #ifdef HAVE_LIBCURL
-#include <curl.h>
-#include <types.h>
-#include <easy.h>
+#include <curl/curl.h>
+#include <curl/types.h>
+#include <curl/easy.h>
 #endif
 
 #ifdef HAVE_LIBCURL
