52c4188cca800f851a6cca4c6921c23d2bfdb1c6
Ingo Ruhnke <grumbel@gmx.de>
2009-11-21 14:33:59 +0000

    Added some build rules for miniswig and the wrapper file
    SVN-Revision: 6078

--- tools/miniswig/create_docu.hpp
+++ tools/miniswig/create_docu.hpp
@@ -11,16 +11,20 @@ public:
     std::ostream& out;
     XmlWriter writer;
 
-    DocuCreator(std::ostream& _out = std::cout)
-        : out(_out), writer(out)
-    {
-        ind = "  ";
-    }
+    DocuCreator(std::ostream& _out = std::cout) :
+        ind("  "),
+        out(_out), 
+        writer(out)
+    { }
 
     void create_docu(Namespace* ns);
     void create_class_docu(Class* _class);
     void create_function_docu(Class* _class, Function* function);
     std::string get_type(const Type& type);
+
+private:
+    DocuCreator(const DocuCreator&);
+    DocuCreator& operator=(const DocuCreator&);
 };
 
 #endif
