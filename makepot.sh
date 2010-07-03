7a60406e641205bdd4ef67e430ef985964f0083f
Mathnerd314 <man.is.allan@gmail.com>
2010-07-03 21:15:56 +0000

    Fix makepot.sh
    SVN-Revision: 6641

--- makepot.sh
+++ makepot.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 xgettext --keyword='_' -C -o data/locale/messages.pot `find src -name "*.cpp"`
-for LEVELSET in "bonus1" "bonus2" "contribs" "misc" "test" "world1" "world2"; do
-  xgettext --keyword='_:1' --language=Lisp --sort-by-file -C -o data/levels/$LEVELSET/messages.pot `find data/levels/$LEVELSET -name "*.stl"` `find data/levels/$LEVELSET -name "*.stwm"` `find data/levels/$LEVELSET -name "*.txt"`
+for LEVELSET in `ls data/levels`; do
+  find data/levels/$LEVELSET "(" -name "*.stl" -or -name "*.stwm" -or -name "*.txt" ")" -print0 | xargs -0 xgettext --keyword='_:1' --language=Lisp --sort-by-file --output data/levels/$LEVELSET/messages.pot
 done
