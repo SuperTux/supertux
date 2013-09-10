0145e0fbb7824c1156c82dcd59302b97995c602e
LMH <lmh.0013@gmail.com>
2013-09-10 12:11:23 -1000

    --amend

--- src/addon/addon_manager.cpp
+++ src/addon/addon_manager.cpp
@@ -104,7 +104,7 @@ AddonManager::check_online()
 #ifdef HAVE_LIBCURL
   char error_buffer[CURL_ERROR_SIZE+1];
 
-  const char* baseUrl = "http://addons.supertux.googlecode.com/git/index-0_3_4.nfo";
+  const char* baseUrl = "http://addons.supertux.googlecode.com/git/index-0_3_5.nfo";
   std::string addoninfos = "";
 
   CURL *curl_handle;
