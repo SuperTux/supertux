c27186a1c306f219968b5f7e1915d000e8047e11
Benjamin Leduc <g.benjamin.leduc@gmail.com>
2014-02-01 11:07:46 +0000

    add a todo

--- makepot.sh
+++ makepot.sh
@@ -3,3 +3,5 @@ xgettext --keyword='_' -C -o data/locale/messages.pot `find src -name "*.cpp"`
 for LEVELSET in `ls data/levels`; do
   find data/levels/$LEVELSET "(" -name "*.stl" -or -name "*.stwm" -or -name "*.txt" ")" -print0 | xargs -0 xgettext --keyword='_:1' --language=Lisp --sort-by-file --output data/levels/$LEVELSET/messages.pot
 done
+
+#todo: make potfiles for addons
