4233c516b92f10a05709a0fcc07a1d2ceb4c4765
Benjamin Leduc <g.benjamin.leduc@gmail.com>
2014-04-26 16:23:10 +0000

    copyright update

--- src/supertux/title_screen.cpp
+++ src/supertux/title_screen.cpp
@@ -57,7 +57,7 @@ TitleScreen::TitleScreen(PlayerStatus* player_status) :
 
   frame = Surface::create("images/engine/menu/frame.png");
   copyright_text = "SuperTux " PACKAGE_VERSION "\n" +
-    _("Copyright") + " (c) 2003-2013 SuperTux Devel Team\n" +
+    _("Copyright") + " (c) 2003-2014 SuperTux Devel Team\n" +
     _("This game comes with ABSOLUTELY NO WARRANTY. This is free software, and you are welcome to\n"
       "redistribute it under certain conditions; see the file COPYING for details.\n"
    );
