9a99bd4bd3df2d8cf8f389eb4b2e2f006108df91
Hume2 <teratux.mail@gmail.com>
2015-07-27 13:10:58 +0200

    Frozen Mr.Tree no longer spawn PoisonIvys when squished

--- src/badguy/mrtree.cpp
+++ src/badguy/mrtree.cpp
@@ -80,24 +80,25 @@ MrTree::collision_squished(GameObject& object)
                                                                    LAYER_OBJECTS-1));
   }
 
-  // spawn PoisonIvy
-  Vector leaf1_pos(stumpy_pos.x - POISONIVY_WIDTH - 1, stumpy_pos.y - POISONIVY_Y_OFFSET);
-  Rectf leaf1_bbox(leaf1_pos.x, leaf1_pos.y, leaf1_pos.x + POISONIVY_WIDTH, leaf1_pos.y + POISONIVY_HEIGHT);
-  if (Sector::current()->is_free_of_movingstatics(leaf1_bbox, this)) {
-    auto leaf1 = std::make_shared<PoisonIvy>(leaf1_bbox.p1, LEFT);
-    leaf1->countMe = false;
-    Sector::current()->add_object(leaf1);
-  }
+	if (!frozen){ //Frozen Mr.Trees don't spawn any PoisonIvys.
+		// spawn PoisonIvy
+		Vector leaf1_pos(stumpy_pos.x - POISONIVY_WIDTH - 1, stumpy_pos.y - POISONIVY_Y_OFFSET);
+		Rectf leaf1_bbox(leaf1_pos.x, leaf1_pos.y, leaf1_pos.x + POISONIVY_WIDTH, leaf1_pos.y + POISONIVY_HEIGHT);
+		if (Sector::current()->is_free_of_movingstatics(leaf1_bbox, this)) {
+			auto leaf1 = std::make_shared<PoisonIvy>(leaf1_bbox.p1, LEFT);
+			leaf1->countMe = false;
+			Sector::current()->add_object(leaf1);
+		}
 
-  // spawn PoisonIvy
-  Vector leaf2_pos(stumpy_pos.x + sprite->get_current_hitbox_width() + 1, stumpy_pos.y - POISONIVY_Y_OFFSET);
-  Rectf leaf2_bbox(leaf2_pos.x, leaf2_pos.y, leaf2_pos.x + POISONIVY_WIDTH, leaf2_pos.y + POISONIVY_HEIGHT);
-  if (Sector::current()->is_free_of_movingstatics(leaf2_bbox, this)) {
-    auto leaf2 = std::make_shared<PoisonIvy>(leaf2_bbox.p1, RIGHT);
-    leaf2->countMe = false;
-    Sector::current()->add_object(leaf2);
-  }
-
+		// spawn PoisonIvy
+		Vector leaf2_pos(stumpy_pos.x + sprite->get_current_hitbox_width() + 1, stumpy_pos.y - POISONIVY_Y_OFFSET);
+		Rectf leaf2_bbox(leaf2_pos.x, leaf2_pos.y, leaf2_pos.x + POISONIVY_WIDTH, leaf2_pos.y + POISONIVY_HEIGHT);
+		if (Sector::current()->is_free_of_movingstatics(leaf2_bbox, this)) {
+			auto leaf2 = std::make_shared<PoisonIvy>(leaf2_bbox.p1, RIGHT);
+			leaf2->countMe = false;
+			Sector::current()->add_object(leaf2);
+		}
+	}
   return true;
 }
 
