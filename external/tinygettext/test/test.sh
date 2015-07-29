adae38f14c3a8438ce4ddea313206e9f878d8f7d
Ingo Ruhnke <grumbel@gmail.com>
2015-07-29 16:05:19 +0200

    Replaced external/tinygetext with proper git submodule
    Since git by default doesn't checkout submodules, existing checkouts
    need to run this to get the submodule:
    
        git submodule update --init --recursive
    
    New checkouts need to be done with:
    
        git clone --recursive https://github.com/SuperTux/supertux.git

--- external/tinygettext/test/test.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-./tinygettext_test translate po/fr.po  "invalid"
-./tinygettext_test directory po/ umlaut Deutsch
-./tinygettext_test directory po/ umlaut deutsch
-./tinygettext_test directory po/ umlaut de
-
-# EOF #
