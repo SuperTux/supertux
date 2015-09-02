4cd2380f005e45a45b96d15616224a60f985e35f
mahboi <mamaluigi1.11.92@gmail.com>
2015-09-02 22:03:32 +0000

    Removed forest world option

--- src/supertux/menu/world_set_menu.hpp
+++ src/supertux/menu/world_set_menu.hpp
@@ -1,3 +1,19 @@
+//  SuperTux
+//  Copyright (C) 2015 Matthew <thebatmankiller3@gmail.com>
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 #ifndef WORLD_SET_MENU_HPP_INCLUDED
 #define WORLD_SET_MENU_HPP_INCLUDED
 
@@ -5,10 +21,8 @@
 
 enum WorldSetMenuIDs
 {
-  WORLDSET_CAMPAIGN1,
-  WORLDSET_CAMPAIGN2,
+  WORLDSET_STORY,
   WORLDSET_CONTRIB
-//WORLDSET_ADDON
 };
 
 class World;
