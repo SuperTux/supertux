588e3078a2da7d9aabc57b99670f3b0ec12db7a5
Tobias Markus <tobbi@mozilla-uk.org>
2015-11-16 00:43:25 +0100

    Fix badguy display for level dispensers

--- src/supertux/statistics.cpp
+++ src/supertux/statistics.cpp
@@ -308,7 +308,7 @@ Statistics::coins_to_string(int coins, int total_coins) {
 std::string
 Statistics::frags_to_string(int badguys, int total_badguys) {
   std::ostringstream os;
-  os << std::min(badguys, 999) << "/" << std::min(total_badguys, 999);
+  os << std::min(std::min(badguys, total_badguys), 999) << "/" << std::min(total_badguys, 999);
   return os.str();
 }
 
