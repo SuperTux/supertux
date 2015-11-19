4d0fa3dbd9bf894a9c65de1dc0b002a5d1048ec5
Tobias Markus <tobbi@mozilla-uk.org>
2015-11-20 00:43:26 +0100

    Change makepot.sh to point to submodule path

--- makepot.sh
+++ makepot.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-xgettext --keyword='_' -C -o data/locale/messages.pot `find src -name "*.cpp"`
+xgettext --keyword='_' -C -o translations/messages.pot `find src -name "*.cpp"`
 for LEVELSET in `ls data/levels`; do
   find data/levels/$LEVELSET "(" -name "*.stl" -or -name "*.stwm" -or -name "*.txt" ")" -print0 | xargs -0 xgettext --keyword='_:1' --language=Lisp --sort-by-file --output translations/$LEVELSET.pot
 done
