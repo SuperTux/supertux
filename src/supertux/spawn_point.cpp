6333e20215b34a14b1f4995d763ade3b18d95dcc
Ingo Ruhnke <grumbel@gmail.com>
2015-11-21 14:46:41 +0100

    Added missing string include

--- src/supertux/spawn_point.cpp
+++ src/supertux/spawn_point.cpp
@@ -15,6 +15,7 @@
 //  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #include <stdexcept>
+#include <string>
 
 #include "supertux/spawn_point.hpp"
 #include "util/log.hpp"
