ab72c72a6b2b940a7ed585bf4edd38d33a9bdaca
Tobias Markus <tobbi@mozilla-uk.org>
2015-11-29 02:49:39 +0100

    Use different syntax for command execution [ci skip]

--- makepot.sh
+++ makepot.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
-xgettext --keyword='_' -C -o translations/messages.pot `find src -name "*.cpp"`
+xgettext --keyword='_' -C -o translations/messages.pot $(find src -name "*.cpp")
 find data/ "(" -name "credits.txt" ")" -print0 | xargs -0 xgettext --keyword='_:1' --language=Lisp --from-code=UTF-8 --sort-by-file --output translations/credits.pot
 
 # Prepare script files for inclusion in tinygettext
-for LEVELSET in `ls data/levels`;do
+for LEVELSET in $(ls data/levels);do
   SCRIPT_FILES=$(find data/levels/$LEVELSET -name "*.nut")
   for SCRIPT_FILE in $SCRIPT_FILES; do
     name=$(basename ${SCRIPT_FILE})
@@ -12,7 +12,7 @@ for LEVELSET in `ls data/levels`;do
   done
 done
 
-for LEVELSET in `ls data/levels`; do
+for LEVELSET in $(ls data/levels); do
   find data/levels/$LEVELSET "(" -name "*.stl" -or -name "*.stwm" -or -name "*.txt" ")" -print0 | xargs -0 xgettext --keyword='_:1' --language=Lisp --sort-by-file --output translations/$LEVELSET.pot
   rm -f data/levels/$LEVELSET/scripts_*.txt 2> /dev/null
 done
