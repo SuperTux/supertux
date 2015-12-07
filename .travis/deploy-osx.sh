6c4166371d8b918a71371bc39ae9e93fe99c7d68
Jacob Burroughs <maths22@gmail.com>
2015-12-07 13:33:33 -0600

    Support OS X deployment

---
+++ .travis/deploy-osx.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+
+if [ "$TRAVIS_OS_NAME" = "osx" ]; then
+    echo -e "Host brlcad.org\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config
+    eval "$(ssh-agent -s)"
+    chmod 600 ../.travis/travis-upload.pem
+    ssh-add ../.travis/travis-upload.pem
+
+    sudo chmod -R +w /usr/local/Cellar
+    cpack -G Bundle;
+    for file in SuperTux-*; do
+        echo "Uploading $file";
+        scp $file travis-upload@brlcad.org:/files/
+    done
+fi
