4478c74e5be6fb354d501ee578f85aa6605f3052
Hume2 <teratux.mail@gmail.com>
2016-01-29 08:53:43 +0100

    Fixed typo in scripting/sector.cpp It's no longer ssector.

--- src/scripting/sector.hpp
+++ src/scripting/sector.hpp
@@ -14,8 +14,8 @@
 //  You should have received a copy of the GNU General Public License
 //  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-#ifndef HEADER_SUPERTUX_SCRIPTING_SSECTOR_HPP
-#define HEADER_SUPERTUX_SCRIPTING_SSECTOR_HPP
+#ifndef HEADER_SUPERTUX_SCRIPTING_SECTOR_HPP
+#define HEADER_SUPERTUX_SCRIPTING_SECTOR_HPP
 
 #ifndef SCRIPTING_API
 #include <string>
