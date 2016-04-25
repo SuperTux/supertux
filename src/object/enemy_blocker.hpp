f6735d81c2d3cf6760af7eb96b6ee4a7326559ff
Savsish <savsish@mail.com>
2016-04-25 12:54:10 +0700

    Create enemy_blocker.hpp

---
+++ src/object/enemy_blocker.hpp
@@ -0,0 +1,40 @@
+//  SuperTux
+//  Copyright (C) 2007 Kovago Zoltan <DirtY.iCE.hu@gmail.com>
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+#ifndef HEADER_SUPERTUX_OBJECT_ENEMY_BLOCKER_HPP
+#define HEADER_SUPERTUX_OBJECT_ENEMY_BLOCKER_HPP
+
+#include "object/moving_sprite.hpp"
+#include "supertux/physic.hpp"
+
+class Player;
+
+/** An object that only solid for badguys. */
+class EnemyBlocker : public MovingSprite
+{
+public:
+  EnemyBlocker(const Reader& lisp);
+
+  HitResponse collision(GameObject& other, const CollisionHit& hit);
+
+private:
+  Physic physic;
+  float width, height;
+};
+
+#endif
+
+/* EOF */
