c3d2b6f7eb91e8f205b0248c2b7576679c1aa379
Hume2 <teratux.mail@gmail.com>
2016-05-26 19:02:10 +0200

    InfoBlock: Make it editor-friendly

--- src/object/infoblock.cpp
+++ src/object/infoblock.cpp
@@ -28,18 +28,13 @@
 #include "video/drawing_context.hpp"
 
 InfoBlock::InfoBlock(const ReaderMapping& lisp) :
-  Block(SpriteManager::current()->create("images/objects/bonus_block/infoblock.sprite")),
+  Block(lisp, "images/objects/bonus_block/infoblock.sprite"),
   message(),
   shown_pct(0),
   dest_pct(0),
   lines(),
   lines_height()
 {
-  Vector pos;
-  if (!lisp.get("x", pos.x)) pos.x = 0;
-  if (!lisp.get("y", pos.y)) pos.y = 0;
-  bbox.set_pos(pos);
-
   if(!lisp.get("message", message)) {
     log_warning << "No message in InfoBlock" << std::endl;
   }
@@ -59,7 +54,7 @@ InfoBlock::~InfoBlock()
 
 void
 InfoBlock::save(Writer& writer) {
-  MovingObject::save(writer);
+  Block::save(writer);
   writer.write("message", message, true);
 }
 
@@ -67,7 +62,7 @@ InfoBlock::save(Writer& writer) {
 ObjectSettings
 InfoBlock::get_settings() {
   ObjectSettings result = Block::get_settings();
-  result.options.push_back( ObjectOption(MN_TEXTFIELD, _("Message"), &message));
+  result.options.push_back( ObjectOption(MN_SCRIPT, _("Message"), &message));
 
   return result;
 }
