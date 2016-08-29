0a44077b30b57378331821255f9eb8b4d7e52c4d
Jacob Burroughs <maths22@gmail.com>
2016-08-29 14:20:38 -0500

    Auto-publish deployment

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -16,5 +16,5 @@ ssh-add ../.travis/travis-upload.pem
 
 for file in SuperTux-*; do
     echo "Uploading $file";
-    curl -T "$file" -umaths22:$BINTRAY_KEY "https://api.bintray.com/content/supertux/SuperTux-Nightly/travis/$TRAVIS_COMMIT/$file"
+    curl -T "$file" -umaths22:$BINTRAY_KEY "https://api.bintray.com/content/supertux/SuperTux-Nightly/travis/$TRAVIS_BUILD_NUMBER/$file?publish=1&override=1"
 done
