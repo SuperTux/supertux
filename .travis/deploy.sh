8119fb711cce7ab901bd070c2bda11bce59bbdc7
Jacob Burroughs <maths22@gmail.com>
2016-08-30 10:24:03 -0500

    Improve travis deployment

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,18 +1,6 @@
 #!/usr/bin/env bash
 
-if [ "$TRAVIS_OS_NAME" = "osx" ]; then
-    sudo chmod -R +w /usr/local/Cellar
-    cpack -G Bundle;
-fi
-
-if [ "$TRAVIS_OS_NAME" = "linux" ] && [ "$CC" = "gcc" ]; then
-    cpack --config CPackSourceConfig.cmake -G TGZ;
-fi
-
-echo -e "Host upload.supertuxproject.org\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config
-eval "$(ssh-agent -s)"
-chmod 600 ../.travis/travis-upload.pem
-ssh-add ../.travis/travis-upload.pem
+shopt -s nullglob
 
 for file in SuperTux-*; do
     echo "Uploading $file";
---
+++ .travis/package.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+shopt -s nullglob
+
+if [ "$TRAVIS_OS_NAME" = "osx" ]; then
+    sudo chmod -R +w /usr/local/Cellar
+    cpack -G Bundle;
+fi
+
+if [ "$TRAVIS_OS_NAME" = "linux" ] && [ "$CC" = "gcc" ]; then
+    cpack --config CPackSourceConfig.cmake -G TGZ;
+fi
+
+for file in SuperTux-*; do
+    echo "Uploading $file";
+    curl --upload-file "$file" "https://transfer.sh/$file"
+done
