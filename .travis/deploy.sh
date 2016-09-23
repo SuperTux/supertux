1e51fb91b8939e12820ac03e654272fab1f9134a
Jacob Burroughs <maths22@gmail.com>
2016-09-23 11:09:25 -0500

    Temporarily disable bintray

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -4,5 +4,5 @@ shopt -s nullglob
 
 for file in SuperTux-*; do
     echo "Uploading $file";
-    curl -T "$file" -umaths22:$BINTRAY_KEY "https://api.bintray.com/content/supertux/SuperTux-Nightly/travis/$TRAVIS_BUILD_NUMBER/$file?publish=1&override=1"
+#    curl -T "$file" -umaths22:$BINTRAY_KEY "https://api.bintray.com/content/supertux/SuperTux-Nightly/travis/$TRAVIS_BUILD_NUMBER/$file?publish=1&override=1"
 done
