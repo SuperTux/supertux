5237e7fbc01736fd636df95915772170755562ef
Max Teufel <max@teufelsnetz.com>
2017-08-19 10:31:59 +0200

    Renderer: Allow 4K viewports
    This is necessary in order to fully be able to use the available resolution on
    modern screens. An effect of this might be that some levels end abruptly at the
    top or bottom edges of the screen, since those were designed for much lower
    resolutions. However, that seems not any worse to me than showing a black edge.
    
    Additionally, the code for badguys has been updated so they are aware of this
    change,

--- src/video/gl/gl_renderer.cpp
+++ src/video/gl/gl_renderer.cpp
@@ -246,7 +246,7 @@ GLRenderer::apply_config()
                                                       target_size);
   }
 
-  Size max_size(1280, 800);
+  Size max_size(3840, 2160);
   Size min_size(640, 480);
 
   Vector scale;
