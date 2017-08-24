b192563b0b14a127c42596e5fd9449fbfbed2966
Tobias Markus <tobbi.bugs@googlemail.com>
2017-08-24 23:41:17 +0200

    Add SQRatObject class

---
+++ src/scripting/sqrat_object.hpp
@@ -0,0 +1,43 @@
+//  SuperTux
+//  Copyright (C) 2017 Tobias Markus <tobbi.bugs@googlemail.com>
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+#ifndef HEADER_SUPERTUX_SCRIPTING_SQRAT_OBJECT_HPP
+#define HEADER_SUPERTUX_SCRIPTING_SQRAT_OBJECT_HPP
+
+
+#include "scripting/scripting.hpp"
+// TODO: Configure using CMake
+#include "../../external/sqrat/include/sqrat.h"
+
+/**
+ * This class defines scripting classes that have the available method to implement SQRat
+ * stuff. This description is just a placeholder anyway.
+ */
+template<class T>
+class SQRatObject
+{
+public:
+    SQRatObject() {}
+    typedef Sqrat::Class<T, Sqrat::NoCopy<T>> SQRatClassType;
+    virtual void register_exposed_methods(HSQUIRRELVM v, SQRatClassType squirrelClass) = 0;
+
+private:
+    SQRatObject(const SQRatObject&) = delete;
+    SQRatObject& operator=(const SQRatObject&) = delete;
+};
+
+#endif
