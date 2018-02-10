7e5dd85b2c79f7b85d4e5788215806bb27641b67
Tobias Markus <tobbi.bugs@googlemail.com>
2018-02-10 03:23:08 +0100

    Use Transifex's capabilities at updating files instead of github repositories

--- makepot.sh
+++ makepot.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-xgettext --keyword='_' -C -o translations/main.pot $(find src -name "*.cpp" -or -name "*.hpp") --add-comments=l10n
-find data/ -name "credits.stxt" -print0 | xargs -0 xgettext --keyword='_:1' --language=Lisp --from-code=UTF-8 --sort-by-file --output translations/credits.pot --add-comments=l10n
-find data/ -name "objects.stoi" -print0 | xargs -0 xgettext --keyword='_:1' --language=Lisp --from-code=UTF-8 --sort-by-file --output translations/objects.pot --add-comments=l10n
-find data/ -name "*.strf" -print0 | xargs -0 xgettext --keyword='_:1' --language=Lisp --from-code=UTF-8 --sort-by-file --output translations/tilesets.pot --add-comments=l10n
+xgettext --keyword='_' -C -o data/main.pot $(find src -name "*.cpp" -or -name "*.hpp") --add-comments=l10n
+find data/ -name "credits.stxt" -print0 | xargs -0 xgettext --keyword='_:1' --language=Lisp --from-code=UTF-8 --sort-by-file --output data/credits.pot --add-comments=l10n
+find data/ -name "objects.stoi" -print0 | xargs -0 xgettext --keyword='_:1' --language=Lisp --from-code=UTF-8 --sort-by-file --output data/objects.pot --add-comments=l10n
+find data/ -name "*.strf" -print0 | xargs -0 xgettext --keyword='_:1' --language=Lisp --from-code=UTF-8 --sort-by-file --output data/tilesets.pot --add-comments=l10n
 
-msgcat translations/main.pot translations/objects.pot translations/tilesets.pot > translations/messages.pot
-rm -f translations/main.pot translations/objects.pot translations/tilesets.pot 2> /dev/null
+msgcat data/main.pot data/objects.pot data/tilesets.pot > data/locale/en.pot
+rm -f data/main.pot data/objects.pot data/tilesets.pot 2> /dev/null
 
 # Prepare script files for inclusion in tinygettext
 for LEVELSET in $(ls data/levels); do
@@ -18,7 +18,7 @@ for LEVELSET in $(ls data/levels); do
 done
 
 for LEVELSET in $(ls data/levels); do
-  find data/levels/$LEVELSET "(" -name "*.stl" -or -name "*.stwm" -or -name "*.txt" ")" -print0 | xargs -0 xgettext --keyword='_:1' --language=Lisp --sort-by-file --output translations/$LEVELSET.pot --add-comments=l10n
-  sed -i -e 's/\\r\\n/\\n/g' translations/$LEVELSET.pot
+  find data/levels/$LEVELSET "(" -name "*.stl" -or -name "*.stwm" -or -name "*.txt" ")" -print0 | xargs -0 xgettext --keyword='_:1' --language=Lisp --sort-by-file --output data/levels/$LEVELSET/en.pot --add-comments=l10n
+  sed -i -e 's/\\r\\n/\\n/g' data/levels/$LEVELSET/en.pot
   rm -f data/levels/$LEVELSET/scripts_*.txt 2> /dev/null
 done
