269e1762ce87346fc45ecfa43104f37516209b70
Allan Nordhøy <epost@anotheragency.no>
2018-02-11 22:22:34 +0100

    fractions for tiles

--- src/supertux/menu/editor_menu.cpp
+++ src/supertux/menu/editor_menu.cpp
@@ -35,9 +35,9 @@ EditorMenu::EditorMenu()
   bool worldmap = Editor::current()->get_worldmap_mode();
   bool is_world = Editor::current()->get_world();
   std::vector<std::string> snap_grid_sizes;
-  snap_grid_sizes.push_back(_("1/8 tile (4px)"));
-  snap_grid_sizes.push_back(_("1/4 tile (8px)"));
-  snap_grid_sizes.push_back(_("1/2 tile (16px)"));
+  snap_grid_sizes.push_back(_("⅛ tile (4px)"));
+  snap_grid_sizes.push_back(_("¼ tile (8px)"));
+  snap_grid_sizes.push_back(_("½ tile (16px)"));
   snap_grid_sizes.push_back(_("1 tile (32px)"));
 
   add_label(_("Level Editor"));
