e11cdd3676a09bc32a69580e89822f38ef9a5935
christ2go <christian@hagemeier.ch>
2018-02-18 20:28:09 +0100

    Add license headers

--- src/supertux/menu/addon_gallery.cpp
+++ src/supertux/menu/addon_gallery.cpp
@@ -1,3 +1,19 @@
+//  SuperTux
+//  Copyright (C) 2018 christ2go <christian@hagemeier.ch>
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 #include "physfs/physfs_file_system.hpp"
 #include "supertux/menu/addon_gallery.hpp"
 #include "util/file_system.hpp"
