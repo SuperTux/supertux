0b985c5fe3624fa0bd6f5c8cbabf1183abb638b5
Tobias Markus <tobbi.bugs@googlemail.com>
2018-06-02 14:06:45 +0200

    Fix crash when adding a dispenser to a level using the level editor

--- src/badguy/dispenser.cpp
+++ src/badguy/dispenser.cpp
@@ -54,7 +54,7 @@ Dispenser::Dispenser(const ReaderMapping& reader) :
   {
     type = dispenser_type_from_string(type_s);
   }
-  catch(std::exception&)
+  catch(...)
   {
     if(!Editor::is_active())
     {
