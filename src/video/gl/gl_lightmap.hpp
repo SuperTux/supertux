ee60aa615e8667d7c1a5dac8a7dc1c8255c33ffa
Ingo Ruhnke <grumbel@gmail.com>
2018-07-21 20:53:24 +0200

    Minor formating cleanup

--- src/video/gl/gl_lightmap.hpp
+++ src/video/gl/gl_lightmap.hpp
@@ -33,15 +33,17 @@ public:
 
   void start_draw(const Color &ambient_color) override;
   void end_draw() override;
-  void do_draw() override;
+
   void draw_surface(const DrawingRequest& request) override;
   void draw_surface_part(const DrawingRequest& request) override;
   void draw_gradient(const DrawingRequest& request) override;
   void draw_filled_rect(const DrawingRequest& request) override;
   void draw_inverse_ellipse(const DrawingRequest& request) override;
-  void get_light(const DrawingRequest& request) const override;
   void draw_line(const DrawingRequest& request) override;
   void draw_triangle(const DrawingRequest& request) override;
+
+  void get_light(const DrawingRequest& request) const override;
+  void do_draw() override;
 
 private:
   static const int s_LIGHTMAP_DIV = 5;
