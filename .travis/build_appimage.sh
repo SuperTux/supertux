ab9b5cd5ecf4eb970ddc03c7e1ceb2631d0a3af2
Max Teufel <max@teufelsnetz.com>
2018-07-25 14:52:16 +0200

    Travis CI: Fix AppImage build
    It has been broken since the icon update. Since the icon name now matches the
    binary name, it should also be possible to replace the custom code with the
    get_icon function.

--- .travis/build_appimage.sh
+++ .travis/build_appimage.sh
@@ -35,18 +35,7 @@ cd $APP.AppDir
 
 get_apprun
 get_desktop
-
-# Our icon filename doesn't match the binary filename, so we can't use the
-# get_icon function here.
-find ./usr/share/pixmaps/supertux.png -exec cp {} . \; 2>/dev/null || true
-find ./usr/share/icons -path *64* -name supertux.png -exec cp {} . \; 2>/dev/null || true
-find ./usr/share/icons -path *128* -name supertux.png -exec cp {} . \; 2>/dev/null || true
-find ./usr/share/icons -path *512* -name supertux.png -exec cp {} . \; 2>/dev/null || true
-find ./usr/share/icons -path *256* -name supertux.png -exec cp {} . \; 2>/dev/null || true
-ls -lh supertux.png || true
-
-# Fix desktop file so it works with generate_type2_appimage
-sed -i 's/Icon=supertux\.png/Icon=supertux/' supertux2.desktop
+get_icon
 
 ########################################################################
 # Copy in the dependencies that cannot be assumed to be available
