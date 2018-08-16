4048414a332c8f44aedab42e3a54aa9d7a2a0168
Ingo Ruhnke <grumbel@gmail.com>
2018-08-17 00:03:15 +0200

    Cleaned up BonusBlock, added m_ prefixes, more 'private:'

--- src/object/bonus_block.hpp
+++ src/object/bonus_block.hpp
@@ -20,23 +20,9 @@
 #include "object/block.hpp"
 #include "object/player.hpp"
 
-class BonusBlock : public Block
+class BonusBlock final : public Block
 {
 public:
-  BonusBlock(const Vector& pos, int data);
-  BonusBlock(const ReaderMapping& lisp);
-  virtual ~BonusBlock();
-  HitResponse collision(GameObject& other, const CollisionHit& hit);
-  virtual void save(Writer& writer);
-
-  void try_open(Player *player);
-  void try_drop(Player *player);
-  std::string get_class() const {
-    return "bonusblock";
-  }
-  std::string get_display_name() const {
-    return _("Bonus block");
-  }
   enum Contents {
     CONTENT_COIN,
     CONTENT_FIREGROW,
@@ -53,28 +39,53 @@ public:
     CONTENT_EXPLODE
   };
 
-  virtual ObjectSettings get_settings();
-
-protected:
-  virtual void hit(Player& player);
-
 public:
-  Contents contents;
-  std::shared_ptr<MovingObject> object;
-  int hit_counter;
-  void draw(DrawingContext& context);
+  BonusBlock(const Vector& pos, int data);
+  BonusBlock(const ReaderMapping& lisp);
+  virtual ~BonusBlock();
+
+  virtual void hit(Player& player) override;
+  virtual HitResponse collision(GameObject& other, const CollisionHit& hit) override;
+  virtual void draw(DrawingContext& context) override;
+
+  Contents get_contents() const { return m_contents; }
+  int get_hit_counter() const { return m_hit_counter; }
+
+  virtual void save(Writer& writer) override;
+
+  virtual std::string get_class() const override {
+    return "bonusblock";
+  }
+  virtual std::string get_display_name() const override {
+    return _("Bonus block");
+  }
+
+  virtual ObjectSettings get_settings() override;
 
 private:
-  BonusBlock(const BonusBlock&);
-  BonusBlock& operator=(const BonusBlock&);
-  std::string script;
-  SurfacePtr lightsprite;
-  BonusBlock::Contents get_content_by_data(int d) const;
+  void try_open(Player* player);
+  void try_drop(Player* player);
+
   void preload_contents(int d);
   void raise_growup_bonus(Player* player, const BonusType& bonus, const Direction& dir);
   void drop_growup_bonus(const std::string& bonus_sprite_name, bool& countdown);
+
+  BonusBlock::Contents get_content_by_data(int d) const;
   BonusBlock::Contents get_content_from_string(const std::string& contentstring) const;
   std::string contents_to_string(const BonusBlock::Contents& content) const;
+
+private:
+  Contents m_contents;
+  std::shared_ptr<MovingObject> m_object;
+  int m_hit_counter;
+
+private:
+  std::string m_script;
+  SurfacePtr m_lightsprite;
+
+private:
+  BonusBlock(const BonusBlock&) = delete;
+  BonusBlock& operator=(const BonusBlock&) = delete;
 };
 
 #endif
