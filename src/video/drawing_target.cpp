b43fa59d6a5c1ba101ef75550856351a6b4a49cd
Ingo Ruhnke <grumbel@gmail.com>
2018-10-04 20:01:20 +0200

    Add DrawingTarget::to_string()/::from_string()

--- src/video/drawing_target.cpp
+++ src/video/drawing_target.cpp
@@ -1,6 +1,5 @@
 //  SuperTux
-//  Copyright (C) 2006 Matthias Braun <matze@braunis.de>
-//                2018 Ingo Ruhnke <grumbel@gmail.com>
+//  Copyright (C) 2018 Ingo Ruhnke <grumbel@gmail.com>
 //
 //  This program is free software: you can redistribute it and/or modify
 //  it under the terms of the GNU General Public License as published by
@@ -15,24 +14,40 @@
 //  You should have received a copy of the GNU General Public License
 //  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-#ifndef HEADER_SUPERTUX_VIDEO_DRAWING_TARGET_HPP
-#define HEADER_SUPERTUX_VIDEO_DRAWING_TARGET_HPP
+#include "video/drawing_target.hpp"
 
-#include <string>
+#include "util/log.hpp"
 
-enum class DrawingTarget {
-  /** The color layer, all regular tilemaps and character sprites go
-      here. */
-  COLORMAP,
+DrawingTarget DrawingTarget_from_string(const std::string& text)
+{
+  if (text == "lightmap")
+  {
+    return DrawingTarget::LIGHTMAP;
+  }
+  else if (text == "colormap")
+  {
+    return DrawingTarget::COLORMAP;
+  }
+  else
+  {
+    log_warning << "unknown drawing target: " << text << std::endl;
+    return DrawingTarget::COLORMAP;
+  }
+}
 
-  /** The lightmap is drawn on top of the color layer and darkens it,
-      elements drawn here act as lightsources. */
-  LIGHTMAP
-};
+std::string to_string(DrawingTarget value)
+{
+  switch(value)
+  {
+    case DrawingTarget::LIGHTMAP:
+      return "lightmap";
 
-DrawingTarget DrawingTarget_from_string(const std::string& text);
-std::string to_string(DrawingTarget value);
+    case DrawingTarget::COLORMAP:
+      return "colormap";
 
-#endif
+    default:
+      return "colormap";
+  }
+}
 
 /* EOF */
