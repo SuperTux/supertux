0096c89acad50b51b9c353e9e80d3007d7c66a4a
Ingo Ruhnke <grumbel@gmail.com>
2018-10-23 22:17:54 +0200

    Fix include path in wrapper generator

--- tools/miniswig/create_wrapper.cpp
+++ tools/miniswig/create_wrapper.cpp
@@ -63,7 +63,7 @@ WrapperCreator::create_wrapper(Namespace* ns)
         << "#include <limits>\n"
         << "#include <sstream>\n"
         << "\n"
-        << "#include \"scripting/squirrel_error.hpp\"\n"
+        << "#include \"squirrel/squirrel_error.hpp\"\n"
         << "#include \"scripting/wrapper.interface.hpp\"\n"
         << "\n"
         << "namespace scripting {\n"
