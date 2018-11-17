2b855e44e5306fbe71d2a300f30f7a99d17bc710
Jacob Burroughs <jburroughs@instructure.com>
2018-11-17 12:36:11 -0600

    Fix s3 uploads

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -3,9 +3,9 @@
 shopt -s nullglob
 
 for file in s3-upload/SuperTux*; do
-    file=$(basename $file)
-    echo "Uploading $file";
-    url="https://supertux-ci-downloads.s3-us-west-2.amazonaws.com/${TRAVIS_COMMIT}/travis/${TRAVIS_OS_NAME}/${TRAVIS_JOB_ID}/$file"
+    file_base=$(basename $file)
+    echo "Uploading $file_base";
+    url="https://supertux-ci-downloads.s3-us-west-2.amazonaws.com/${TRAVIS_COMMIT}/travis/${TRAVIS_OS_NAME}/${TRAVIS_JOB_ID}/$file_base"
     size=$(($(wc -c < "$file")))
     shasum=$(shasum -a 256 "$file" | cut -d " " -f 1)
     curl --data "apikey=$DOWNLOAD_APIKEY" \
