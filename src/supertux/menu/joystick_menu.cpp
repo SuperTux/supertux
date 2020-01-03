1789d48930bc54cf830cfa993f140ad6fb54e8ef
Tobias Markus <tobbi.bugs@googlemail.com>
2020-01-03 03:47:45 +0100

    Fix code style in joystick_menu.cpp [ci skip] [appveyor skip]

--- src/supertux/menu/joystick_menu.cpp
+++ src/supertux/menu/joystick_menu.cpp
@@ -79,8 +79,6 @@ JoystickMenu::recreate_menu()
       add_controlfield(static_cast<int>(Control::PEEK_DOWN),   _("Peek Down"));
       if (g_config->developer_mode) {
         add_controlfield(static_cast<int>(Control::CONSOLE), _("Console"));
-      }
-      if (g_config->developer_mode) {
         add_controlfield(static_cast<int>(Control::CHEAT_MENU), _("Cheat Menu"));
         add_controlfield(static_cast<int>(Control::DEBUG_MENU), _("Debug Menu"));
       }
