3911ffddbcf6739a23353fc0f455909565af7de2
Tobias Markus <tobbi.bugs@googlemail.com>
2020-01-03 02:58:53 +0100

    Change ScriptTrigger's layer to LAYER_OBJECTS
    Fixes #969

--- src/trigger/scripttrigger.cpp
+++ src/trigger/scripttrigger.cpp
@@ -115,7 +115,7 @@ ScriptTrigger::draw(DrawingContext& context)
 {
   if (Editor::is_active() || g_debug.show_collision_rects) {
     context.color().draw_filled_rect(m_col.m_bbox, Color(1.0f, 0.0f, 1.0f, 0.6f),
-                             0.0f, LAYER_GUI);
+                             0.0f, LAYER_OBJECTS);
   }
 }
 
