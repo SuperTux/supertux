5d0587e97937b4ab416a79c4d603b95862aab8b9
Tobias Markus <tobbi.bugs@googlemail.com>
2020-06-11 21:23:28 +0200

    Fix raqm configuring step with newer CMake

--- mk/cmake/FindRAQM.cmake
+++ mk/cmake/FindRAQM.cmake
@@ -1,8 +1,8 @@
 include(FindPkgConfig)
 pkg_check_modules(PC_RAQM raqm)
-find_path(RAQM_INCLUDE_DIRS NAMES raqm.h HINTS ${PC_RAQM_INCLUDE_DIRS}
+find_path(RAQM_INCLUDE_DIR NAMES raqm.h HINTS ${PC_RAQM_INCLUDE_DIRS}
   ${PC_RAQM_INCLUDEDIR})
-find_library(RAQM_LIBRARIES NAMES raqm HINTS ${PC_RAQM_LIBRARY_DIRS}
+find_library(RAQM_LIBRARY NAMES raqm HINTS ${PC_RAQM_LIBRARY_DIRS}
   ${PC_RAQM_LIBDIR})
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(RAQM DEFAULT_MSG RAQM_INCLUDE_DIRS RAQM_LIBRARIES)
+find_package_handle_standard_args(RAQM DEFAULT_MSG RAQM_INCLUDE_DIR RAQM_LIBRARY)
