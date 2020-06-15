3a2082200a9bc29786c5993cc6b87335edf8da03
Rudolf Polzer <divVerent@gmail.com>
2020-06-15 06:10:59 -0700

    Make the other Rectf constructor also assert that size cannot be negative.
    Helped locate and fix the "crash on dragging rectangles up or left" bug.

--- src/math/rectf.hpp
+++ src/math/rectf.hpp
@@ -49,6 +49,8 @@ public:
   Rectf(const Vector& np1, const Vector& np2) :
     m_p1(np1), m_size(np2.x - np1.x, np2.y - np1.y)
   {
+    assert(m_size.width >= 0 &&
+           m_size.height >= 0);
   }
 
   Rectf(float x1, float y1, float x2, float y2) :
