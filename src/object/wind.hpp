06c60dcf3e738c540cc7353dbafa5e9533383de4
weluvgoatz <39805490+weluvgoatz@users.noreply.github.com>
2020-08-06 16:17:45 -0700

    Updated wind

--- src/object/wind.hpp
+++ src/object/wind.hpp
@@ -61,6 +61,7 @@ private:
   float dt_sec; /**< stores last dt_sec gotten at update() */
 
   bool affects_badguys; /**< whether the wind can affect badguys */
+  bool fancy_wind;
 
 private:
   Wind(const Wind&) = delete;
