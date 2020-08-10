89fbde7020626aac26be5f6cea3868a04518b213
omeritzics <66558205+omeritzics@users.noreply.github.com>
2020-08-10 23:01:06 +0300

    Update resources.cpp

--- src/supertux/resources.cpp
+++ src/supertux/resources.cpp
@@ -91,9 +91,6 @@ Resources::get_font_for_locale(const std::string& locale)
     return "fonts/NotoSansDevanagari-Medium.ttf";
   if(locale == "cmn" || locale == "ja" || locale == "zh_CN" || locale == "zh_TW")
     return "fonts/NotoSansCJKjp-Medium.otf";
-  if(locale == "he")
-    return "fonts/Yehuda CLM Bold.ttf";
-  return "fonts/SuperTux-Medium.ttf";
 }
 
 void
