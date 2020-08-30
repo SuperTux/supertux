3e37efebb3f0a24bacd44c20d489177470de5f1e
Omer I.S <66558205+omeritzics@users.noreply.github.com>
2020-08-31 00:28:10 +0300

    Update resources.cpp

--- src/supertux/resources.cpp
+++ src/supertux/resources.cpp
@@ -87,13 +87,13 @@ Resources::load()
 std::string
 Resources::get_font_for_locale(const std::string& locale)
 {
+  return "fonts/SuperTux-Medium.ttf";
   if(locale == "ne")
     return "fonts/NotoSansDevanagari-Medium.ttf";
   if(locale == "cmn" || locale == "ja" || locale == "zh_CN" || locale == "zh_TW")
     return "fonts/NotoSansCJKjp-Medium.otf";
   if(locale == "he")
     return "fonts/shuneet3-medium.otf";
-  return "fonts/SuperTux-Medium.ttf";
 }
 
 void
