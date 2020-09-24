56dd66083ca4a7e81ddb1b36c243e03f2cb62dd4
Tobias Markus <tobbi.bugs@googlemail.com>
2020-09-24 21:12:38 +0200

    More instances of git describe --always

--- .travis/build_appimage.sh
+++ .travis/build_appimage.sh
@@ -14,7 +14,7 @@ LOWERAPP=supertux2
 GIT_REV=$(git rev-parse --short HEAD)
 echo $GIT_REV
 
-RELEASE_VERSION=$(git describe --tags)
+RELEASE_VERSION=$(git describe --tags --always)
 
 make install DESTDIR=$HOME/$APP/$APP.AppDir
 
