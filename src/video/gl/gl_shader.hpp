3f6342fa525f6af5a18a1a0eeebe9ad828c820b2
Tobias Markus <tobbi.bugs@googlemail.com>
2020-11-26 00:27:40 +0100

    Add missing include guard in gl_shader.hpp

--- src/video/gl/gl_shader.hpp
+++ src/video/gl/gl_shader.hpp
@@ -14,6 +14,9 @@
 //  You should have received a copy of the GNU General Public License
 //  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+#ifndef HEADER_SUPERTUX_VIDEO_GL_GL_SHADER_HPP
+#define HEADER_SUPERTUX_VIDEO_GL_GL_SHADER_HPP
+
 #include <memory>
 #include <string>
 #include <vector>
@@ -47,5 +50,7 @@ private:
   GLShader(const GLShader&) = delete;
   GLShader& operator=(const GLShader&) = delete;
 };
+
+#endif
 
 /* EOF */
