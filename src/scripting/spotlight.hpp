9a9b58e96222aceda408f81c58a138524c5ca929
Semphris <semphris@protonmail.com>
2021-03-07 11:01:41 -0500

    Fixed copyright headers

--- src/scripting/spotlight.hpp
+++ src/scripting/spotlight.hpp
@@ -1,5 +1,5 @@
-//  SuperTux - Sector scripting
-//  Copyright (C) 2016 Hume2 <teratux.mail@gmail.com>
+//  SuperTux
+//  Copyright (C) 2021 A. Semphris <semphris@protonmail.com>
 //
 //  This program is free software: you can redistribute it and/or modify
 //  it under the terms of the GNU General Public License as published by
