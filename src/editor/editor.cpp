129ed5e95c05fe9cf880610a0a3cb1473ede005e
Carsten Wirtz <carstenwirtz@live.de>
2021-08-02 12:33:30 +0200

    Change level editor background
    Use version of arctis2 background with sky for the level editor start screen.

--- src/editor/editor.cpp
+++ src/editor/editor.cpp
@@ -100,7 +100,7 @@ Editor::Editor() :
   m_toolbox_widget(),
   m_layers_widget(),
   m_enabled(false),
-  m_bgr_surface(Surface::from_file("images/background/antarctic/arctis2.png")),
+  m_bgr_surface(Surface::from_file("images/engine/menu/bg_editor.png")),
   m_undo_manager(new UndoManager),
   m_ignore_sector_change(false),
   m_level_first_loaded(false),
