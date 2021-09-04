ab327f318b4a83fbf348d94c3679c3a9125726bd
mrkubax10 <mrkubax10@onet.pl>
2021-09-04 09:20:45 +0200

    Fix text anchor points

--- src/object/text_object.cpp
+++ src/object/text_object.cpp
@@ -140,7 +140,7 @@ TextObject::draw(DrawingContext& context)
 
   float width  = m_font->get_text_width(m_wrapped_text) + 20.0f;
   float height = m_font->get_text_height(m_wrapped_text) + 20.0f;
-  Vector spos = m_pos + get_anchor_pos(Rectf(0, 0, static_cast<float>(context.get_width()), static_cast<float>(context.get_height() + SCREEN_HEIGHT) - 340.0f),
+  Vector spos = m_pos + get_anchor_pos(Rectf(0, 0, static_cast<float>(context.get_width()), static_cast<float>(context.get_height() + (m_anchor == ANCHOR_MIDDLE ? SCREEN_HEIGHT : 0)) - (m_anchor == ANCHOR_MIDDLE ? 340.0f : 0)),
                                        width, height, m_anchor);
 
   context.color().draw_filled_rect(Rectf(spos, Sizef(width, height)),
