a29188b4267eebb68f3787fd3a492256e74de094
Tobias Markus <tobbi.bugs@googlemail.com>
2021-11-07 23:53:01 +0100

    Another deploy fix (maybe)

--- .ci_scripts/deploy.sh
+++ .ci_scripts/deploy.sh
@@ -8,10 +8,11 @@ for file in upload/SuperTux*; do
     url="https://supertux-ci-downloads.s3-us-west-2.amazonaws.com/${PREFIX}/$file_base"
     size=$(($(wc -c < "$file")))
     if [ $IS_WINDOWS = true ] ; then
-        shasum=$(powershell -command Get-FileHash $file -Algorithm SHA256)
+        shasum=$(powershell -command "Get-FileHash \"$file\" -Algorithm SHA256")
     else
         shasum=$(shasum -a 256 "$file" | cut -d " " -f 1)
     fi
+    echo "Checksum: $shasum";
     curl --data "apikey=$DOWNLOAD_APIKEY" \
          --data "url=$url" \
          --data "size=$size" \
