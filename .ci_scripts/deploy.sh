b1bf186303710cbf1865dd89f22dea2c317aad17
Tobias Markus <tobbi.bugs@googlemail.com>
2021-11-07 21:29:20 +0100

    More verbose upload output

--- .ci_scripts/deploy.sh
+++ .ci_scripts/deploy.sh
@@ -6,6 +6,7 @@ for file in upload/SuperTux*; do
     file_base=$(basename $file)
     echo "Uploading $file_base";
     url="https://supertux-ci-downloads.s3-us-west-2.amazonaws.com/${PREFIX}/$file_base"
+    echo "Upload file to $url";
     size=$(($(wc -c < "$file")))
     shasum=$(shasum -a 256 "$file" | cut -d " " -f 1)
     curl --data "apikey=$DOWNLOAD_APIKEY" \
