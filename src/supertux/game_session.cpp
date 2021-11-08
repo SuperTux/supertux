cd14ff0664ddca6005124446b4426c9170b88b50
HybridDog <3192173+HybridDog@users.noreply.github.com>
2021-11-08 19:35:17 +0100

    When pressing ESC after dying at a level start, ignore the play time if left or right is hold (#1852)
    * When pressing ESC after dying at a level start, ignore the play time if left or right is hold
    
    The play time condition is required so that it is possible to leave a level where Tux immediately dies in the beginning.
    If Tux nonetheless dies near the beginning, pressing ESC while holding left or right now enables the player to quickly respawn.
    
    * use a bool parameter

--- src/supertux/game_session.cpp
+++ src/supertux/game_session.cpp
@@ -167,9 +167,11 @@ GameSession::restart_level(bool after_death)
 }
 
 void
-GameSession::on_escape_press()
+GameSession::on_escape_press(bool force_quick_respawn)
 {
-  if ((m_currentsector->get_player().is_dying() && m_play_time > 2.0f) || m_end_sequence)
+  if ((m_currentsector->get_player().is_dying() && (m_play_time > 2.0f
+      || force_quick_respawn))
+    || m_end_sequence)
   {
     // Let the timers run out, we fast-forward them to force past a sequence
     if (m_end_sequence)
@@ -331,7 +333,8 @@ GameSession::update(float dt_sec, const Controller& controller)
   if (controller.pressed(Control::ESCAPE) ||
       controller.pressed(Control::START))
   {
-    on_escape_press();
+    on_escape_press(controller.hold(Control::LEFT)
+      || controller.hold(Control::RIGHT));
   }
 
   if (controller.pressed(Control::CHEAT_MENU) &&
