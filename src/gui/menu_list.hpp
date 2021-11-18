facfbb6539deb411553da149fb19f5a8b7cdc940
Rami-Slicer <rami.slicer@gmail.com>
2021-11-18 10:59:29 -0600

    Add the license info
    Fix trailing underscore

--- src/gui/menu_list.hpp
+++ src/gui/menu_list.hpp
@@ -1,3 +1,19 @@
+//  SuperTux -- List menu
+//  Copyright (C) 2021 Rami <rami.slicer@gmail.com>
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 #ifndef HEADER_SUPERTUX_GUI_MENU_LIST_HPP
 #define HEADER_SUPERTUX_GUI_MENU_LIST_HPP
 
@@ -6,7 +22,7 @@
 class ListMenu final : public Menu 
 {
 public:
-  ListMenu(const std::vector<std::string>& items, int* selected_);
+  ListMenu(const std::vector<std::string>& items, int* selected);
 
   void menu_action(MenuItem& item);
 
