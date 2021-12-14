72ef0f22fa5a7216b45104244014c77924cb5ebf
Semphris <semphris@protonmail.com>
2021-12-14 16:50:27 -0500

    Testing a level by pressing Ctrl+T no longer messes up the autotile config. See #1923 on GitHub.

--- src/editor/editor.cpp
+++ src/editor/editor.cpp
@@ -315,6 +315,7 @@ Editor::get_level_directory() const
 void
 Editor::test_level(const boost::optional<std::pair<std::string, Vector>>& test_pos)
 {
+  m_overlay_widget->reset_action_press();
 
   Tile::draw_editor_images = false;
   Compositor::s_render_lighting = true;
