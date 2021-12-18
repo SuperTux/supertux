SuperTux/supertux

commit b61b1ced26edd332211fd299c4991745986e95e0

Refactor level flipping (#1922)
* Refactor level flipping

* Use reference in transform_flip, don't use const pointer

* Fix compilation

src/badguy/badguy.cpp
@@ -394,6 +394,14 @@ BadGuy::collision_solid(const CollisionHit& hit)
   update_on_ground_flag(hit);
 }
 
+void
+BadGuy::on_flip(float height)
+{
+  Vector pos = get_start_position();
+  pos.y = height - pos.y;
+  set_start_position(pos);
+}
+
 HitResponse
 BadGuy::collision_player(Player& player, const CollisionHit& )
 {

src/object/block.cpp
@@ -231,4 +231,10 @@ void Block::after_editor_set()
   m_sprite = SpriteManager::current()->create(m_sprite_name);
 }
 
+void
+Block::on_flip(float height)
+{
+  if (m_original_y != -1) m_original_y = height - m_original_y - get_bbox().get_height();
+}
+
 /* EOF */

src/object/decal.cpp
@@ -16,7 +16,7 @@
 
 #include "object/decal.hpp"
 #include "scripting/decal.hpp"
-
+#include "supertux/flip_level_transformer.hpp"
 #include "sprite/sprite_manager.hpp"
 #include "util/reader.hpp"
 #include "util/reader_mapping.hpp"
@@ -101,6 +101,12 @@ Decal::fade_sprite(const std::string& new_sprite, float fade_time)
   // From now on flip_sprite == the old one
   m_sprite.get()->set_alpha(0);
   m_sprite_timer.start(fade_time);
+}
+
+void
+Decal::on_flip(float height)
+{
+  FlipLevelTransformer::transform_flip(m_flip);
 }
 
 void

src/object/flower.cpp
@@ -20,6 +20,7 @@
 #include "object/player.hpp"
 #include "sprite/sprite.hpp"
 #include "sprite/sprite_manager.hpp"
+#include "supertux/flip_level_transformer.hpp"
 
 Flower::Flower(BonusType _type) :
   type(_type),
@@ -81,6 +82,12 @@ Flower::collision(GameObject& other, const CollisionHit& )
   SoundManager::current()->play("sounds/fire-flower.wav");
   remove_me();
   return ABORT_MOVE;
+}
+
+void
+Flower::on_flip(float height)
+{
+  FlipLevelTransformer::transform_flip(flip);
 }
 
 /* EOF */

src/object/platform.cpp
@@ -19,6 +19,7 @@
 #include "editor/editor.hpp"
 #include "object/player.hpp"
 #include "supertux/sector.hpp"
+#include "supertux/flip_level_transformer.hpp"
 #include "util/reader_mapping.hpp"
 #include "util/writer.hpp"
 
@@ -143,6 +144,14 @@ Platform::editor_update()
     m_starting_node = static_cast<int>(get_path()->get_nodes().size()) - 1;
 
   set_pos(get_path()->get_nodes()[m_starting_node].position);
+}
+
+void
+Platform::on_flip(float height)
+{
+  if (Path* path = get_path()) {
+    FlipLevelTransformer::transform_path(height, get_bbox().get_height(), *path);
+  }
 }
 
 void

src/badguy/badguy.hpp
@@ -150,6 +150,8 @@ protected:
   /** Called when the badguy collided with solid ground */
   virtual void collision_solid(const CollisionHit& hit) override;
 
+  virtual void on_flip(float height) override;
+
   /** Called when the badguy collided with another badguy */
   virtual HitResponse collision_badguy(BadGuy& other, const CollisionHit& hit);
 

src/object/block.hpp
@@ -41,6 +41,8 @@ public:
   virtual ObjectSettings get_settings() override;
   virtual void after_editor_set() override;
 
+  virtual void on_flip(float height) override;
+
 protected:
   virtual void hit(Player& player) = 0;
 

src/object/decal.hpp
@@ -45,6 +45,8 @@ public:
   virtual void draw(DrawingContext& context) override;
   virtual void update(float dt_sec) override;
 
+  virtual void on_flip(float height) override;
+
   void fade_in(float fade_time);
   void fade_out(float fade_time);
   void fade_sprite(const std::string& new_sprite, float fade_time);

commit 95f3932961922edff71c4a510c154abafaf28c04

Fix issues introduced in #1922

src/badguy/badguy.cpp
@@ -397,6 +397,7 @@ BadGuy::collision_solid(const CollisionHit& hit)
 void
 BadGuy::on_flip(float height)
 {
+  MovingObject::on_flip(height);
   Vector pos = get_start_position();
   pos.y = height - pos.y;
   set_start_position(pos);

src/object/block.cpp
@@ -234,6 +234,7 @@ void Block::after_editor_set()
 void
 Block::on_flip(float height)
 {
+  MovingObject::on_flip(height);
   if (m_original_y != -1) m_original_y = height - m_original_y - get_bbox().get_height();
 }
 

src/object/decal.cpp
@@ -106,6 +106,7 @@ Decal::fade_sprite(const std::string& new_sprite, float fade_time)
 void
 Decal::on_flip(float height)
 {
+  MovingObject::on_flip(height);
   FlipLevelTransformer::transform_flip(m_flip);
 }
 

src/object/flower.cpp
@@ -87,6 +87,7 @@ Flower::collision(GameObject& other, const CollisionHit& )
 void
 Flower::on_flip(float height)
 {
+  MovingObject::on_flip(height);
   FlipLevelTransformer::transform_flip(flip);
 }
 

src/object/platform.cpp
@@ -149,6 +149,7 @@ Platform::editor_update()
 void
 Platform::on_flip(float height)
 {
+  MovingObject::on_flip(height);
   if (Path* path = get_path()) {
     FlipLevelTransformer::transform_path(height, get_bbox().get_height(), *path);
   }

src/trigger/door.cpp
@@ -24,6 +24,7 @@
 #include "supertux/game_session.hpp"
 #include "supertux/screen_manager.hpp"
 #include "supertux/sector.hpp"
+#include "supertux/flip_level_transformer.hpp"
 #include "util/reader_mapping.hpp"
 
 Door::Door(const ReaderMapping& mapping) :
@@ -33,7 +34,8 @@ Door::Door(const ReaderMapping& mapping) :
   target_spawnpoint(),
   script(),
   sprite(SpriteManager::current()->create("images/objects/door/door.sprite")),
-  stay_open_timer()
+  stay_open_timer(),
+  m_flip(NO_FLIP)
 {
   mapping.get("x", m_col.m_bbox.get_left());
   mapping.get("y", m_col.m_bbox.get_top());
@@ -55,7 +57,8 @@ Door::Door(int x, int y, const std::string& sector, const std::string& spawnpoin
   target_spawnpoint(spawnpoint),
   script(),
   sprite(SpriteManager::current()->create("images/objects/door/door.sprite")),
-  stay_open_timer()
+  stay_open_timer(),
+  m_flip(NO_FLIP)
 {
   m_col.m_bbox.set_pos(Vector(static_cast<float>(x), static_cast<float>(y)));
 
@@ -117,7 +120,7 @@ Door::update(float )
 void
 Door::draw(DrawingContext& context)
 {
-  sprite->draw(context.color(), m_col.m_bbox.p1(), LAYER_BACKGROUNDTILES+1);
+  sprite->draw(context.color(), m_col.m_bbox.p1(), LAYER_BACKGROUNDTILES+1, m_flip);
 }
 
 void
@@ -177,6 +180,13 @@ Door::collision(GameObject& other, const CollisionHit& hit_)
   }
 
   return TriggerBase::collision(other, hit_);
+}
+
+void
+Door::on_flip(float height)
+{
+  MovingObject::on_flip(height);
+  FlipLevelTransformer::transform_flip(m_flip);
 }
 
 /* EOF */

src/trigger/door.hpp
@@ -19,6 +19,7 @@
 
 #include "supertux/timer.hpp"
 #include "trigger/trigger_base.hpp"
+#include "video/flip.hpp"
 
 class Player;
 class ReaderMapping;
@@ -39,6 +40,7 @@ public:
   virtual void draw(DrawingContext& context) override;
   virtual void event(Player& player, EventType type) override;
   virtual HitResponse collision(GameObject& other, const CollisionHit& hit) override;
+  virtual void on_flip(float height) override;
 
 private:
   enum DoorState {
@@ -55,6 +57,7 @@ private:
   std::string script;
   SpritePtr sprite; /**< "door" sprite to render */
   Timer stay_open_timer; /**< time until door will close again */
+  Flip m_flip;
 
 private:
   Door(const Door&) = delete;