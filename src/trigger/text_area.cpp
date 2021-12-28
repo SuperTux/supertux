6bb879115a77ad008f1aaa834bb2ad6742e8bfee
Semphris <semphris@protonmail.com>
2021-12-28 05:14:10 -0500

    Added my name to the copyright header (TextArea)

--- src/trigger/text_area.cpp
+++ src/trigger/text_area.cpp
@@ -1,5 +1,6 @@
 //  SuperTux
 //  Copyright (C) 2021 mrkubax10 <mrkubax10@onet.pl>
+//                2021 A. Semphris <semphris@protonmail.com>
 //
 //  This program is free software: you can redistribute it and/or modify
 //  it under the terms of the GNU General Public License as published by
