f0a372ced8e54ab0a96c277e2c5a8842afead7d9
Semphris <semphris@protonmail.com>
2021-12-29 18:19:32 -0500

    Fixed copyright headers

--- src/supertux/menu/multiplayer_player_menu.cpp
+++ src/supertux/menu/multiplayer_player_menu.cpp
@@ -1,5 +1,5 @@
 //  SuperTux
-//  Copyright (C) 2015 Matthew <thebatmankiller3@gmail.com>
+//  Copyright (C) 2021 A. Semphris <semphris@protonmail.com>
 //
 //  This program is free software: you can redistribute it and/or modify
 //  it under the terms of the GNU General Public License as published by
