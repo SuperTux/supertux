bc4f58ae26e48f09355298aa0a88b76a1cbd1ba4
Jiří Paleček <narre@protonmail.com>
2022-01-01 15:27:02 +0100

    Code consistency

--- src/object/fallblock.cpp
+++ src/object/fallblock.cpp
@@ -32,9 +32,9 @@ FallBlock::FallBlock(const ReaderMapping& reader) :
   physic(),
   timer()
 {
-	SoundManager::current()->preload("sounds/cracking.wav");
-	SoundManager::current()->preload("sounds/thud.ogg");
-	physic.enable_gravity(false);
+  SoundManager::current()->preload("sounds/cracking.wav");
+  SoundManager::current()->preload("sounds/thud.ogg");
+  physic.enable_gravity(false);
 }
 
 void
@@ -43,20 +43,20 @@ FallBlock::update(float dt_sec)
   switch (state)
   {
     case IDLE:
-			set_group(COLGROUP_STATIC);
-	    if (found_victim_down())
-		  {
+      set_group(COLGROUP_STATIC);
+      if (found_victim_down())
+      {
         state = SHAKE;
-	      SoundManager::current()->play("sounds/cracking.wav");
-	      timer.start(0.5f);
-		  }
+        SoundManager::current()->play("sounds/cracking.wav");
+        timer.start(0.5f);
+      }
       break;
     case SHAKE:
-	    if (timer.check())
+      if (timer.check())
       {
         state = FALL;
-	      physic.reset();
-		    physic.enable_gravity(true);
+        physic.reset();
+        physic.enable_gravity(true);
       }
       break;
     case FALL:
@@ -65,32 +65,32 @@ FallBlock::update(float dt_sec)
       break;
     case LAND:
       m_col.set_movement(physic.get_movement (dt_sec));
-	    set_group(COLGROUP_MOVING_STATIC);
+      set_group(COLGROUP_MOVING_STATIC);
       break;
   }
   for (auto& bumper : Sector::get().get_objects_by_type<Bumper>())
   {
-	  Rectf bumper_bbox = bumper.get_bbox();
-	  if ((bumper_bbox.get_left() < (m_col.m_bbox.get_right() + 8))
-	  && (bumper_bbox.get_right() > (m_col.m_bbox.get_left() - 8))
+    Rectf bumper_bbox = bumper.get_bbox();
+    if ((bumper_bbox.get_left() < (m_col.m_bbox.get_right() + 8))
+    && (bumper_bbox.get_right() > (m_col.m_bbox.get_left() - 8))
     && (bumper_bbox.get_bottom() > (m_col.m_bbox.get_top() - 8))
-	  && (bumper_bbox.get_top() < (m_col.m_bbox.get_bottom() + 8)))
+    && (bumper_bbox.get_top() < (m_col.m_bbox.get_bottom() + 8)))
     {
       switch (state)
       {
-	      case IDLE:
-	        break;
-	      case SHAKE:
-	        break;
-	      case FALL:
-	        bumper.physic.enable_gravity(true);
-	        break;
-	      case LAND:
-	        bumper.physic.enable_gravity(false);
-	        bumper.physic.set_gravity_modifier(0.f);
-	        bumper.physic.set_velocity_y(0.f);
-	        bumper.physic.reset();
-	        break;
+        case IDLE:
+          break;
+        case SHAKE:
+          break;
+        case FALL:
+          bumper.physic.enable_gravity(true);
+          break;
+        case LAND:
+          bumper.physic.enable_gravity(false);
+          bumper.physic.set_gravity_modifier(0.f);
+          bumper.physic.set_velocity_y(0.f);
+          bumper.physic.reset();
+          break;
       }
     }
   }
@@ -99,21 +99,21 @@ FallBlock::update(float dt_sec)
 HitResponse
 FallBlock::collision(GameObject& other, const CollisionHit& hit)
 {
-	auto fallblock = dynamic_cast<FallBlock*> (&other);
+  auto fallblock = dynamic_cast<FallBlock*> (&other);
   if (fallblock && hit.bottom && (state == FALL || state == LAND))
   {
-	  physic.set_velocity_y(0.0f);
-	  return CONTINUE;
+    physic.set_velocity_y(0.0f);
+    return CONTINUE;
   }
 
-	auto player = dynamic_cast<Player*>(&other);
-	if (state == IDLE && player && player->get_bbox().get_bottom() < m_col.m_bbox.get_top())
+  auto player = dynamic_cast<Player*>(&other);
+  if (state == IDLE && player && player->get_bbox().get_bottom() < m_col.m_bbox.get_top())
   {
-	  state = SHAKE;
-	  SoundManager::current()->play("sounds/cracking.wav");
-	  timer.start(0.5f);
-	}
-	return FORCE_MOVE;
+    state = SHAKE;
+    SoundManager::current()->play("sounds/cracking.wav");
+    timer.start(0.5f);
+  }
+  return FORCE_MOVE;
 }
 
 void
@@ -135,16 +135,14 @@ FallBlock::collision_solid(const CollisionHit& hit)
 void
 FallBlock::draw(DrawingContext& context)
 {
-
   Vector pos = get_pos();
   // shaking
   if (state == SHAKE)
   {
     pos.x += static_cast<float>(graphicsRandom.rand(-8, 8));
-	  pos.y += static_cast<float>(graphicsRandom.rand(-5, 5));
+    pos.y += static_cast<float>(graphicsRandom.rand(-5, 5));
   }
   MovingSprite::draw(context);
-
 }
 
 bool
