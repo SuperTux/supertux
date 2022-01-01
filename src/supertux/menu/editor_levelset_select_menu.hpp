fc09fc75cb947db4adb19167cf3e47b89d03b7b5
mrkubax10 <mrkubax10@onet.pl>
2022-01-01 13:20:25 +0100

    Use const reference

--- src/supertux/menu/editor_levelset_select_menu.hpp
+++ src/supertux/menu/editor_levelset_select_menu.hpp
@@ -32,7 +32,7 @@ public:
   void initialize();
   void reload_menu();
 
-  std::vector<std::string>& get_contrib_worlds() { return m_contrib_worlds; }
+  const std::vector<std::string>& get_contrib_worlds() { return m_contrib_worlds; }
 
 private:
   EditorLevelsetSelectMenu(const EditorLevelsetSelectMenu&) = delete;
