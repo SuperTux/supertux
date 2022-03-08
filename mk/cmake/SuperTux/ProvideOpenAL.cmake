229b7ae87393a62c91db5c084318901802cb5bd1
Ingo Ruhnke <grumbel@gmail.com>
2022-03-08 22:26:47 +0100

    Use OpenAL target across all build configuration

--- mk/cmake/SuperTux/ProvideOpenAL.cmake
+++ mk/cmake/SuperTux/ProvideOpenAL.cmake
@@ -1,12 +1,23 @@
 if(NOT EMSCRIPTEN)
   if(VCPKG_BUILD)
     find_package(OpenAL CONFIG REQUIRED)
+    add_library(OpenAL ALIAS OpenAL::OpenAL)
   else()
     find_package(OpenAL REQUIRED)
+
+    add_library(OpenAL INTERFACE)
+    set_target_properties(OpenAL PROPERTIES
+      INTERFACE_LINK_LIBRARIES "${OPENAL_LIBRARY}"
+      INTERFACE_INCLUDE_DIRECTORIES "${OPENAL_INCLUDE_DIR}"
+      INTERFACE_SYSTEM_INCLUDE_DIRECTORIES "${OPENAL_INCLUDE_DIR}"
+      )
   endif()
-  include_directories(SYSTEM ${OPENAL_INCLUDE_DIR})
 else()
-  include_directories(${CMAKE_SOURCE_DIR}/mk/emscripten/AL)
+  add_library(OpenAL INTERFACE)
+  set_target_properties(OpenAL PROPERTIES
+    INTERFACE_INCLUDE_DIRECTORIES "${CMAKE_SOURCE_DIR}/mk/emscripten/AL"
+    INTERFACE_LINK_LIBRARIES "-lopenal"
+    )
 endif()
 
 mark_as_advanced(
