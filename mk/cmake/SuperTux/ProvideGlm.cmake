194ddf76b34903cb89a48dc293abff38b90012f7
Ingo Ruhnke <grumbel@gmail.com>
2022-03-11 00:39:13 +0100

    Replace glm::glm target with LibGlm, add -DGLM_ENABLE_EXPERIMENTAL

--- mk/cmake/SuperTux/ProvideGlm.cmake
+++ mk/cmake/SuperTux/ProvideGlm.cmake
@@ -3,13 +3,21 @@ find_package(glm QUIET)
 if(glm_FOUND)
   if(TARGET glm::glm)
     message(STATUS "Found glm")
-    add_library(LibGlm ALIAS glm::glm)
+
+    add_library(LibGlm INTERFACE IMPORTED)
+    set_target_properties(LibGlm PROPERTIES
+      INTERFACE_LINK_LIBRARIES "glm::glm"
+      INTERFACE_SYSTEM_INCLUDE_DIRECTORIES "$<TARGET_PROPERTY:glm::glm,INTERFACE_INCLUDE_DIRECTORIES>"
+      INTERFACE_COMPILE_DEFINITIONS "GLM_ENABLE_EXPERIMENTAL")
   else()
     # fallback for old glm version in EmuELEC and Nix
     message(STATUS "Found glm: ${GLM_INCLUDE_DIR}")
+
     add_library(LibGlm INTERFACE IMPORTED)
     set_target_properties(LibGlm PROPERTIES
-      INTERFACE_INCLUDE_DIRECTORIES "$<TARGET_PROPERTY:glm,INTERFACE_INCLUDE_DIRECTORIES>")
+      INTERFACE_LINK_LIBRARIES "glm"
+      INTERFACE_SYSTEM_INCLUDE_DIRECTORIES "$<TARGET_PROPERTY:glm,INTERFACE_INCLUDE_DIRECTORIES>"
+      INTERFACE_COMPILE_DEFINITIONS "GLM_ENABLE_EXPERIMENTAL")
   endif()
 else()
   # fallback for old glm version in UBPorts
@@ -23,7 +31,8 @@ else()
   message(STATUS "Found glm: ${GLM_INCLUDE_DIR}")
   add_library(LibGlm INTERFACE IMPORTED)
   set_target_properties(LibGlm PROPERTIES
-    INTERFACE_INCLUDE_DIRECTORIES ${GLM_INCLUDE_DIR})
+    INTERFACE_INCLUDE_DIRECTORIES "${GLM_INCLUDE_DIR}"
+    INTERFACE_COMPILE_DEFINITIONS "GLM_ENABLE_EXPERIMENTAL")
 endif()
 
 # EOF #
