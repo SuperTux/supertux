e55f33e8c72037ecd03f3bec890c00145597c794
Ingo Ruhnke <grumbel@gmail.com>
2022-03-15 21:26:03 +0100

    Fix debug/optimized keyword issue on CMake/Win32

--- mk/cmake/SuperTux/ProvideCurl.cmake
+++ mk/cmake/SuperTux/ProvideCurl.cmake
@@ -21,8 +21,13 @@ if(NOT EMSCRIPTEN)
 
   add_library(LibCurl INTERFACE IMPORTED)
   set_target_properties(LibCurl PROPERTIES
-    INTERFACE_LINK_LIBRARIES "${CURL_LIBRARY}"
     INTERFACE_INCLUDE_DIRECTORIES "${CURL_INCLUDE_DIRS}")
+  if(WIN32)
+    target_link_libraries(LibCurl INTERFACE ${CURL_LIBRARY})
+  else()
+    set_target_properties(LibCurl PROPERTIES
+      INTERFACE_LINK_LIBRARIES "${CURL_LIBRARY}")
+  endif()
 
   set(HAVE_LIBCURL TRUE)
 endif()
