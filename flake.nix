43bec80c5481aed4d7ff750e853d6c6ef1cba47f
Ingo Ruhnke <grumbel@gmail.com>
2022-08-22 13:38:23 +0200

    Add mingw cross-compiling to flake.nix

--- flake.nix
+++ flake.nix
@@ -25,44 +25,109 @@
 
   inputs = rec {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.05";
-    flake-utils.url = "github:numtide/flake-utils";
+
+    tinycmmc.url = "github:grumbel/tinycmmc";
+    tinycmmc.inputs.nixpkgs.follows = "nixpkgs";
+
+    SDL-win32.url = "github:grumnix/SDL-win32";
+    SDL-win32.inputs.nixpkgs.follows = "nixpkgs";
+    SDL-win32.inputs.tinycmmc.follows = "tinycmmc";
+
+    SDL_mixer-win32.url = "github:grumnix/SDL_mixer-win32";
+    SDL_mixer-win32.inputs.nixpkgs.follows = "nixpkgs";
+    SDL_mixer-win32.inputs.tinycmmc.follows = "tinycmmc";
+    SDL_mixer-win32.inputs.SDL-win32.follows = "SDL-win32";
+
+    SDL_image-win32.url = "github:grumnix/SDL_image-win32";
+    SDL_image-win32.inputs.nixpkgs.follows = "nixpkgs";
+    SDL_image-win32.inputs.tinycmmc.follows = "tinycmmc";
+    SDL_image-win32.inputs.SDL-win32.follows = "SDL-win32";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = nixpkgs.legacyPackages.${system};
-      in rec {
-        packages = flake-utils.lib.flattenTree rec {
+  outputs = { self, nixpkgs, tinycmmc, SDL-win32, SDL_mixer-win32, SDL_image-win32 }:
+    tinycmmc.lib.eachSystemWithPkgs (pkgs:
+      {
+        packages = rec {
+          default = supertux-milestone1;
+
           supertux-milestone1 = pkgs.stdenv.mkDerivation rec {
             pname = "supertux-milestone1";
             version = "0.1.4";
+
             src = nixpkgs.lib.cleanSource ./.;
+
             enableParallelBuilding = true;
+
             nativeBuildInputs = [
-              pkgs.autoconf
-              pkgs.automake
-              pkgs.autoreconfHook
-              pkgs.gcc
-              pkgs.makeWrapper
-              pkgs.pkgconfig
-            ];
-            postFixup = ''
-                wrapProgram $out/bin/supertux-milestone1 \
-                    --prefix LIBGL_DRIVERS_PATH ":" "${pkgs.mesa.drivers}/lib/dri" \
-                    --prefix LD_LIBRARY_PATH ":" "${pkgs.mesa.drivers}/lib"
-            '';
-            buildInputs = [
-              pkgs.SDL
-              pkgs.SDL_image
-              pkgs.SDL_mixer
-              pkgs.libGL
-              pkgs.libGLU
-              pkgs.zlib
-            ];
-           };
+              pkgs.buildPackages.autoconf
+              pkgs.buildPackages.automake
+              pkgs.buildPackages.autoreconfHook
+              pkgs.buildPackages.pkgconfig
+            ]
+            ++ (nixpkgs.lib.optional pkgs.targetPlatform.isLinux pkgs.makeWrapper);
+
+            postFixup = ""
+              + (nixpkgs.lib.optionalString pkgs.targetPlatform.isLinux ''
+                   wrapProgram $out/bin/supertux-milestone1 \
+                     --prefix LIBGL_DRIVERS_PATH ":" "${pkgs.mesa.drivers}/lib/dri" \
+                     --prefix LD_LIBRARY_PATH ":" "${pkgs.mesa.drivers}/lib"
+                 '')
+              + (nixpkgs.lib.optionalString pkgs.stdenv.targetPlatform.isWindows ''
+                   mkdir -p $out/bin/
+                   find ${pkgs.windows.mcfgthreads} -iname "*.dll" -exec ln -sfv {} $out/bin/ \;
+                   find ${pkgs.stdenv.cc.cc} -iname "*.dll" -exec ln -sfv {} $out/bin/ \;
+                   ln -sfv ${SDL-win32.packages.${pkgs.system}.default}/bin/*.dll $out/bin/
+                   ln -sfv ${SDL_image-win32.packages.${pkgs.system}.default}/bin/*.dll $out/bin/
+                   ln -sfv ${SDL_mixer-win32.packages.${pkgs.system}.default}/bin/*.dll $out/bin/
+
+                   # FIXME: These should be handled in their respective libraries
+                   ln -sfv ${pkgs.libpng}/bin/*.dll $out/bin/
+                   ln -sfv ${(pkgs.libjpeg_original.overrideAttrs (oldAttrs: { meta = {}; }))}/bin/*.dll $out/bin/
+                   ln -sfv ${pkgs.zlib}/bin/*.dll $out/bin/
+                 '');
+
+            buildInputs =
+              (if pkgs.stdenv.targetPlatform.isWindows
+               then [
+                 SDL-win32.packages.${pkgs.system}.default
+                 SDL_image-win32.packages.${pkgs.system}.default
+                 SDL_mixer-win32.packages.${pkgs.system}.default
+               ]
+               else [
+                 pkgs.SDL
+                 pkgs.SDL_image
+                 pkgs.SDL_mixer
+                 pkgs.libGL
+                 pkgs.libGLU
+               ]) ++
+              [
+                pkgs.zlib
+              ];
+          };
+
+          supertux-milestone1-win32 = pkgs.runCommand "supertux-milestone1-win32" {} ''
+            mkdir -p $out
+            mkdir -p $out/data/
+
+            cp -vr ${supertux-milestone1}/bin/supertux-milestone1.exe $out/
+            cp -vLr ${supertux-milestone1}/bin/*.dll $out/
+            cp -vr ${supertux-milestone1}/share/supertux-milestone1/. $out/data/
+          '';
+
+          supertux-milestone1-win32-zip = pkgs.runCommand "supertux-milestone1-win32-zip" {} ''
+            mkdir -p $out
+            WORKDIR=$(mktemp -d)
+
+            cp --no-preserve mode,ownership --verbose --recursive \
+              ${supertux-milestone1-win32}/. "$WORKDIR"
+
+            cd "$WORKDIR"
+            ${nixpkgs.legacyPackages.x86_64-linux.zip}/bin/zip \
+              -r \
+              $out/supertux-milestone1-${supertux-milestone1.version}-${pkgs.system}.zip \
+              .
+          '';
         };
-        defaultPackage = packages.supertux-milestone1;
       }
     );
 }
