fbe7283ad7a3999c0a7a6cdd028006028a402a95
Leonid Pospelov <leonidpospelov.dev@gmail.com>
2023-03-20 00:28:50 +0600

    [WIP] Update wasm.yml (PR to your PR) (#2390)
    * Try to diagnose vcpkg build errors on wasm
    
    * Build against latest emscripten
    
    * Trigger wasm
    
    * Use latest test branch from my fork
    
    * Fix syntax
    
    * Switch branch the proper way
    
    * Maybe this works now
    
    * Update wasm.yml
    
    * Trigger another build
    
    * Add required file
    
    * Show issue description
    
    * Check cloning b2-wasm-fix
    
    * Force CMAKE_C_COMPILER and CMAKE_CXX_COMPILER
    
    * Revert "Check cloning b2-wasm-fix"
    
    This reverts commit e545850dc5ac4cb2b61e05b7210afb8dbe43d3b4.
    
    * Hardcode CMAKE_C_COMPILER and CMAKE_CXX_COMPILER
    
    * full path to emsdk
    
    * Fix flags for latest emscripten
    
    * Another fix
    
    * Remove USE_ES2 flag
    
    * Remove another unused parameter
    
    * Remove -SSAFE_HEAP=1
    
    * Remove -SAssertions=1
    
    * Remove -SDEMANGLE_SUPPORT flag
    
    * Remove -sERROR_ON_UNDEFINED_SYMBOLS
    
    * Remove --preload-file
    
    * Remove --use-preload-plugins
    
    * Don't build with -WError
    
    * Update wasm.yml
    
    * Update wasm.yml
    
    * no emcmake/emmake
    
    * attempt to fix find_boost
    
    ---------
    
    Co-authored-by: Tobias Markus <tobbi.bugs@googlemail.com>

--- mk/cmake/SuperTux/ProvideBoost.cmake
+++ mk/cmake/SuperTux/ProvideBoost.cmake
@@ -1,44 +1,45 @@
-set(Boost_ADDITIONAL_VERSIONS "1.41" "1.41.0")
+add_library(LibBoost INTERFACE IMPORTED)
 
-if(WIN32)
-  option(ENABLE_BOOST_STATIC_LIBS "Use boost static libraries" ON)
-else()
-  option(ENABLE_BOOST_STATIC_LIBS "Use boost static libraries" OFF)
-endif()
+if(NOT EMSCRIPTEN)
+  set(Boost_ADDITIONAL_VERSIONS "1.41" "1.41.0")
+  
+  if(WIN32)
+    option(ENABLE_BOOST_STATIC_LIBS "Use boost static libraries" ON)
+  else()
+    option(ENABLE_BOOST_STATIC_LIBS "Use boost static libraries" OFF)
+  endif()
 
-if(ENABLE_BOOST_STATIC_LIBS)
-  set(Boost_USE_STATIC_LIBS TRUE)
-else()
-  set(Boost_USE_STATIC_LIBS FALSE)
-endif()
-
-find_package(Boost REQUIRED COMPONENTS filesystem system date_time locale)
-
-if(EMSCRIPTEN)
-  add_library(LibBoost ALIAS Boost::boost)
-else()
-  add_library(LibBoost INTERFACE IMPORTED)
-endif()
-
-if(WIN32)
+  if(ENABLE_BOOST_STATIC_LIBS)
+    set(Boost_USE_STATIC_LIBS TRUE)
+  else()
+    set(Boost_USE_STATIC_LIBS FALSE)
+  endif()
+  
+  find_package(Boost REQUIRED COMPONENTS filesystem system date_time locale)
+  
+  if(WIN32)
   # Boost_LIBRARIES may contain link-type keywords "optimized,debug"
   # that aren't understood by INTERFACE_LINK_DIRECTORIES directly,
   # meanwhile older cmake on other OSs don't support
   # target_link_libraries(INTERFACE).
-  target_link_libraries(LibBoost INTERFACE ${Boost_LIBRARIES})
-else()
+    target_link_libraries(LibBoost INTERFACE ${Boost_LIBRARIES})
+  else()
+    set_target_properties(LibBoost PROPERTIES
+      INTERFACE_LINK_LIBRARIES "${Boost_LIBRARIES}")
+  endif()
+
   set_target_properties(LibBoost PROPERTIES
-    INTERFACE_LINK_LIBRARIES "${Boost_LIBRARIES}")
+    INTERFACE_LINK_DIRECTORIES "${Boost_LIBRARY_DIRS}"
+    INTERFACE_INCLUDE_DIRECTORIES "${Boost_INCLUDE_DIR}"
+   )
+
+  mark_as_advanced(
+    Boost_INCLUDE_DIR
+    Boost_LIBRARIES
+  )
+else()
+  find_package(Boost REQUIRED)
+  target_link_libraries(LibBoost INTERFACE Boost::boost)
 endif()
-
-set_target_properties(LibBoost PROPERTIES
-  INTERFACE_LINK_DIRECTORIES "${Boost_LIBRARY_DIRS}"
-  INTERFACE_INCLUDE_DIRECTORIES "${Boost_INCLUDE_DIR}"
-  )
-
-mark_as_advanced(
-  Boost_INCLUDE_DIR
-  Boost_LIBRARIES
-  )
 
 # EOF #
