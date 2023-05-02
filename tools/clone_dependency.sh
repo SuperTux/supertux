6d2f92a3aa172d16eedc866ec3eb6718b121e667
MatusGuy <85036874+MatusGuy@users.noreply.github.com>
2023-05-02 11:01:02 +0100

    Merge 6f8f45e34

---
+++ tools/clone_dependency.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/bash
+# Script used by GitHub workflows (mainly android)
+# to clone a git repository
+
+git clone
+  "https://github.com/$1"
+  --recurse-submodules
+  --branch $2
+  --depth 1
