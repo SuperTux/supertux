f843590468a4c34083e62c0bbe403f9f655276c9
MatusGuy <85036874+MatusGuy@users.noreply.github.com>
2023-05-03 20:32:55 +0100

    supertux android specific files

---
+++ mk/android/app/src/main/org/lethargik/supertux2/MainActivity.java
@@ -0,0 +1,24 @@
+package matusguy.game.mousemaze;
+
+import org.libsdl.app.*;
+
+public class MainActivity extends SDLActivity {
+    @Override
+    protected String[] getLibraries() {
+        return new String[] {
+            "SDL2",
+            "SDL2_image",
+            "SDL2_ttf",
+            "supertux2"
+        };
+    }
+
+    @Override
+    protected String getMainSharedObject() {
+        return "libsupertux2.so";
+    }
+    @Override
+    protected String getMainFunction() {
+        return "SDL_main";
+    }
+}
