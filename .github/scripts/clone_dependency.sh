9b288f6d5abeb615a8b09c9cbc1e270861b5c902
MatusGuy <85036874+MatusGuy@users.noreply.github.com>
2023-05-03 12:01:44 +0100

    clone dep script must be 1 line

--- .github/scripts/clone_dependency.sh
+++ .github/scripts/clone_dependency.sh
@@ -2,8 +2,4 @@
 # Script used by GitHub workflows (mainly android)
 # to clone a git repository
 
-git clone
-  "https://github.com/$1"
-  --recurse-submodules
-  --branch $2
-  --depth 1
+git clone "https://github.com/$1" --recurse-submodules --branch $2 --depth 1
