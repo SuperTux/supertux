470be66917782f8a5607329269d5a6803fea46ba
MatusGuy <85036874+MatusGuy@users.noreply.github.com>
2023-05-03 10:33:40 +0100

    Give permission to run clone deps script
    Bruh

--- tools/clone_dependency.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/bash
-# Script used by GitHub workflows (mainly android)
-# to clone a git repository
-
-git clone
-  "https://github.com/$1"
-  --recurse-submodules
-  --branch $2
-  --depth 1
