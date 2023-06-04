da35e81c1a07414dfa2e6e5395d8de505dc8e91d
MatusGuy <85036874+MatusGuy@users.noreply.github.com>
2023-06-04 20:57:47 +0100

    finally!! openssl on android!

--- mk/android/app/jni/openssl/include/openssl/configuration.h
+++
@@ -1,4 +0,0 @@
-// this is another one of those "the build system will fill this in"
-// except unlike ogg/config_types.h this one is almost impossible to fill in
-#define OPENSSL_CONFIGURED_API 30000
-#define SIXTY_FOUR_BIT_LONG
