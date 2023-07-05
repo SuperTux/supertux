db1043a50757c7e6ca9864a4b915e9c20ae165c2
Carsten Wirtz <carstenwirtz@live.de>
2023-07-05 21:20:13 +0200

    Fine-tune Dive Mine movement 2/?

--- src/badguy/dive_mine.cpp
+++ src/badguy/dive_mine.cpp
@@ -23,8 +23,8 @@
 #include "sprite/sprite_manager.hpp"
 #include "supertux/sector.hpp"
 
-const float DiveMine::s_trigger_radius = 80.f;
-const float DiveMine::s_swim_speed = 15.f;
+const float DiveMine::s_trigger_radius = 100.f;
+const float DiveMine::s_swim_speed = 20.f;
 const float DiveMine::s_max_float_acceleration = 15.f;
 
 DiveMine::DiveMine(const ReaderMapping& reader) :
