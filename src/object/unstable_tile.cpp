a7ece5f6d91efa2a1c832fc7c02c26c70052512e
MatusGuy <85036874+MatusGuy@users.noreply.github.com>
2023-07-12 23:08:22 +0100

    Decrease respawn recognition hitbox by 1
    So it can stop recognizing static objects next to each other, for example

--- src/object/unstable_tile.cpp
+++ src/object/unstable_tile.cpp
@@ -198,7 +198,7 @@ UnstableTile::update(float dt_sec)
       {
         if (m_revive_timer.check())
         {
-          if (Sector::get().is_free_of_movingstatics(Rectf(m_original_pos, get_bbox().get_size())))
+          if (Sector::get().is_free_of_movingstatics(Rectf(m_original_pos, get_bbox().get_size()).grown(-1.f)))
           {
             revive();
           }
