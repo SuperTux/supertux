fa8f06825d96e793eac4c437ac640f8be6d77934
Jiří Paleček <narre@protonmail.com>
2023-07-15 23:32:07 +0200

    make the replace tool capable of replacing with a selection instead of just a single tile

--- src/editor/overlay_widget.cpp
+++ src/editor/overlay_widget.cpp
@@ -573,7 +573,7 @@ EditorOverlayWidget::replace()
     {
       if (tilemap->get_tile_id(x, y) == replace_tile)
       {
-        tilemap->change(x, y, m_editor.get_tiles()->pos(0, 0));
+        tilemap->change(x, y, m_editor.get_tiles()->pos(x % m_editor.get_tiles()->m_width, y % m_editor.get_tiles()->m_height));
       }
     }
   }
