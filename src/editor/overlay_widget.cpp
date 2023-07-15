f83b2a31556ada35f386cdd1816e14566798b510
Jiří Paleček <narre@protonmail.com>
2023-07-16 01:03:28 +0200

    make the clicked tile the top left of the selection that's replacing it

--- src/editor/overlay_widget.cpp
+++ src/editor/overlay_widget.cpp
@@ -573,7 +573,8 @@ EditorOverlayWidget::replace()
     {
       if (tilemap->get_tile_id(x, y) == replace_tile)
       {
-        tilemap->change(x, y, m_editor.get_tiles()->pos(x % m_editor.get_tiles()->m_width, y % m_editor.get_tiles()->m_height));
+        tilemap->change(x, y, m_editor.get_tiles()->pos((x + static_cast<int>(m_hovered_tile.x)) % m_editor.get_tiles()->m_width,
+                                                        (y + static_cast<int>(m_hovered_tile.y)) % m_editor.get_tiles()->m_height));
       }
     }
   }
