ebf3d1223087814b189ec0a095be3e284bb63349
Tobias Markus <tobbi.bugs@googlemail.com>
2023-07-19 01:48:59 +0200

    Change assertion to if statement to prevent crashes while calling from scripting

--- src/object/tilemap.cpp
+++ src/object/tilemap.cpp
@@ -677,7 +677,9 @@ TileMap::get_tile_at(const Vector& pos) const
 void
 TileMap::change(int x, int y, uint32_t newtile)
 {
-  assert(x >= 0 && x < m_width && y >= 0 && y < m_height);
+  if(x < 0 || x >= m_width || y < 0 || y >= m_height)
+    return;
+
   m_tiles[y*m_width + x] = newtile;
 }
 
