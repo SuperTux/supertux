cf5313c5378f50f62911962222b0e871f19602e6
Tobias Markus <tobbi.bugs@googlemail.com>
2023-07-23 22:31:35 +0200

    Add set_action to Decal scripting Fixes #2557

--- src/scripting/wrapper.cpp
+++ src/scripting/wrapper.cpp
@@ -4920,6 +4920,36 @@ static SQInteger Decal_fade_out_wrapper(HSQUIRRELVM vm)
 
 }
 
+static SQInteger Decal_set_action_wrapper(HSQUIRRELVM vm)
+{
+  SQUserPointer data;
+  if(SQ_FAILED(sq_getinstanceup(vm, 1, &data, nullptr, SQTrue)) || !data) {
+    sq_throwerror(vm, _SC("'set_action' called without instance"));
+    return SQ_ERROR;
+  }
+  scripting::Decal* _this = reinterpret_cast<scripting::Decal*> (data);
+
+  const SQChar* arg0;
+  if(SQ_FAILED(sq_getstring(vm, 2, &arg0))) {
+    sq_throwerror(vm, _SC("Argument 1 not a string"));
+    return SQ_ERROR;
+  }
+
+  try {
+    _this->set_action(arg0);
+
+    return 0;
+
+  } catch(std::exception& e) {
+    sq_throwerror(vm, e.what());
+    return SQ_ERROR;
+  } catch(...) {
+    sq_throwerror(vm, _SC("Unexpected exception while executing function 'set_action'"));
+    return SQ_ERROR;
+  }
+
+}
+
 static SQInteger Dispenser_release_hook(SQUserPointer ptr, SQInteger )
 {
   scripting::Dispenser* _this = reinterpret_cast<scripting::Dispenser*> (ptr);
@@ -14949,6 +14979,13 @@ void register_supertux_wrapper(HSQUIRRELVM v)
   sq_setparamscheck(v, SQ_MATCHTYPEMASKSTRING, ".b|n");
   if(SQ_FAILED(sq_createslot(v, -3))) {
     throw SquirrelError(v, "Couldn't register function 'fade_out'");
+  }
+
+  sq_pushstring(v, "set_action", -1);
+  sq_newclosure(v, &Decal_set_action_wrapper, 0);
+  sq_setparamscheck(v, SQ_MATCHTYPEMASKSTRING, ".s");
+  if(SQ_FAILED(sq_createslot(v, -3))) {
+    throw SquirrelError(v, "Couldn't register function 'set_action'");
   }
 
   if(SQ_FAILED(sq_createslot(v, -3))) {
