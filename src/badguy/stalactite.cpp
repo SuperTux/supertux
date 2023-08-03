bfef9989e5d1319ec318cd2f84f78041ceadddb2
Carsten Wirtz <carstenwirtz@live.de>
2023-08-03 19:11:37 +0200

    Fix error in stalactite.cpp

--- src/badguy/stalactite.cpp
+++ src/badguy/stalactite.cpp
@@ -163,7 +163,7 @@ void
 Stalactite::on_type_change(int old_type)
 {
   if (!has_found_sprite()) // Change sprite only if a custom sprite has not just been loaded.
-    change_sprite("images/creatures/stalactite/"stalactite_ + std::string(m_type == StalactiteType::ROCK ? "rock" : "") + ".sprite");
+    change_sprite("images/creatures/stalactite/stalactite_" + std::string(m_type == StalactiteType::ROCK ? "rock" : "") + ".sprite");
 }
 
 void
