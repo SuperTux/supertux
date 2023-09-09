22a1bfb9a5a36f74d03d95fd11a3637fa85f5049
Tobias Markus <tobbi.bugs@googlemail.com>
2023-09-09 22:48:45 +0200

    Maybe fix

--- src/editor/object_option.hpp
+++ src/editor/object_option.hpp
@@ -78,7 +78,7 @@ class ObjectOption: public BaseObjectOption
 {
 public:
   ObjectOption(const std::string& text, const std::string& key, unsigned int flags, T* pointer = NULL);
-  ~ObjectOption();
+  ~ObjectOption() override;
 
   virtual T* get_value() const { return m_value_pointer; }
 
