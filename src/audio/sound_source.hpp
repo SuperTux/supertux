42fbdf99bf9562a45fcc4d2fc112ed8e872509eb
mrkubax10 <mrkubax10@onet.pl>
2023-09-16 13:06:21 +0200

    Bomb: Play bomb_tick.ogg simultaneously with fizz.wav

--- src/audio/sound_source.hpp
+++ src/audio/sound_source.hpp
@@ -29,6 +29,8 @@ public:
   virtual ~SoundSource() {}
 
   virtual void play() = 0;
+  virtual void pause() = 0;
+  virtual void resume() = 0;
   virtual void stop() = 0;
   virtual bool playing() const = 0;
 
