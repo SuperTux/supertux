765911487117dff02a5eb7d6199941b092757ffc
MatusGuy <85036874+MatusGuy@users.noreply.github.com>
2023-12-14 20:46:23 +0000

    Prevent climbing if rock tux
    fixes #2693

--- src/object/player.cpp
+++ src/object/player.cpp
@@ -2504,7 +2504,7 @@ Player::set_ghost_mode(bool enable)
 void
 Player::start_climbing(Climbable& climbable)
 {
-  if (m_climbing || m_swimming)
+  if (m_climbing || m_swimming || m_stone)
     return;
 
   m_climbing = &climbable;
