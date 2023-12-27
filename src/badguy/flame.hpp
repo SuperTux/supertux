2f4fd634ce20269da0df50c3923d9a7a681ee6cc
Vankata453 <78196474+Vankata453@users.noreply.github.com>
2023-12-28 01:45:45 +0200

    This too [ci skip]

--- src/badguy/flame.hpp
+++ src/badguy/flame.hpp
@@ -30,7 +30,7 @@ public:
   virtual void deactivate() override;
 
   virtual void active_update(float dt_sec) override;
-  virtual void draw(DrawingContext &context) override;
+  virtual void draw(DrawingContext& context) override;
   virtual void kill_fall() override;
 
   virtual void freeze() override;
