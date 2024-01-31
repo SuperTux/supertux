4712a397af11c0384a83d93e066e8c8889121c97
Vankata453 <78196474+Vankata453@users.noreply.github.com>
2024-01-31 15:09:40 +0200

    Change copyright year to 2024: pt. 2 [ci skip]

--- src/object/snow_particle_system.hpp
+++ src/object/snow_particle_system.hpp
@@ -1,6 +1,6 @@
 //  SuperTux
 //  Copyright (C) 2006 Matthias Braun <matze@braunis.de>
-//  Copyright (C) 2023 bruhmoent
+//  Copyright (C) 2024 bruhmoent
 //
 //  This program is free software: you can redistribute it and/or modify
 //  it under the terms of the GNU General Public License as published by
