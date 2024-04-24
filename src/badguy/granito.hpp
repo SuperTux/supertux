49f2d25a62882a92b9324f83f3f01911e008c090
MatusGuy <85036874+MatusGuy@users.noreply.github.com>
2024-04-24 20:32:45 +0100

    prevent granito being pushed by explosions

--- src/badguy/granito.hpp
+++ src/badguy/granito.hpp
@@ -56,6 +56,7 @@ public:
   virtual bool is_snipable() const override { return false; }
   virtual bool is_freezable() const override { return false; }
   virtual bool is_flammable() const override { return false; }
+  virtual bool is_heavy() const override { return true; }
 
   virtual void kill_fall() override;
 
