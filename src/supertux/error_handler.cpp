389900a0fdbb4732a348fbb32c67990f912b7e0b
Tobias Markus <tobbi.bugs@googlemail.com>
2024-05-01 09:52:23 +0200

    Don't escape already escaped string

--- src/supertux/error_handler.cpp
+++ src/supertux/error_handler.cpp
@@ -342,7 +342,7 @@ void ErrorHandler::report_error(const std::string& details)
 {
   std::string sysinfo = get_system_info();
   std::stringstream urlbuilder;
-  std::string labels = FileSystem::escape("type:crash,status:needs-confirmation");
+  std::string labels = "type:crash,status:needs-confirmation";
   std::stringstream bodybuilder;
   bodybuilder << "**SuperTux version:** *" PACKAGE_VERSION "*\r\n"
                  "**System information:** *" << sysinfo << "*\r\n"
