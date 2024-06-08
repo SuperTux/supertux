7b1aadd9ae6f88bc7a92ffb9e133abab1086b113
Vankata453 <78196474+Vankata453@users.noreply.github.com>
2024-06-08 13:46:10 +0300

    Documentation generation improvements

--- mk/cmake/SuperTux/BuildDocumentation.cmake
+++ mk/cmake/SuperTux/BuildDocumentation.cmake
@@ -2,7 +2,7 @@
 # Adapted from https://tty1.net/blog/2014/cmake-doxygen_en.html
 # add a target to generate API documentation with Doxygen
 option(BUILD_DOCUMENTATION "Build API documentation using Doxygen" ${DOXYGEN_FOUND})
-option(BUILD_SCRIPTING_DOCUMENTATION "Build scripting API documentation using Doxygen" ${DOXYGEN_FOUND})
+option(BUILD_DOCUMENTATION_WITH_SCRIPTING "Build API documentation, including scripting API, using Doxygen" OFF)
 
 # CMakeLists, including this file from a different directory, can define PROJECT_SOURCE_DIR as the path to the SuperTux source.
 if(NOT DEFINED PROJECT_SOURCE_DIR)
@@ -16,7 +16,7 @@ if(BUILD_DOCUMENTATION OR BUILD_DOCUMENTATION_WITH_SCRIPTING)
   if(BUILD_DOCUMENTATION_WITH_SCRIPTING)
     set(doxyfile_in ${PROJECT_SOURCE_DIR}/mk/doxygen/Doxyfile_scripting.in)
   else()
-    set(doxyfile_in ${PROJECT_SOURCE_DIR}/Doxyfile.in)
+    set(doxyfile_in ${PROJECT_SOURCE_DIR}/mk/doxygen/Doxyfile.in)
   endif()
   set(doxyfile ${CMAKE_CURRENT_BINARY_DIR}/docs/Doxyfile)
 
