c8a80f95ffaa6b16c739710230ab123c201cc8ed
Vankata453 <78196474+Vankata453@users.noreply.github.com>
2024-07-31 03:24:11 +0300

    Add descriptions to functions in `WorldMapSector` scripting docs [ci skip]

--- src/worldmap/worldmap_sector.hpp
+++ src/worldmap/worldmap_sector.hpp
@@ -110,32 +110,32 @@ public:
 
   /**
    * @scripting
-   * Returns Tux's X position on the worldmap.
+   * @description Returns Tux's X position on the worldmap.
    */
   float get_tux_x() const;
   /**
    * @scripting
-   * Returns Tux's Y position on the worldmap.
+   * @description Returns Tux's Y position on the worldmap.
    */
   float get_tux_y() const;
 
   /**
    * @scripting
-   * Changes the current sector of the worldmap to a specified new sector.
+   * @description Changes the current sector of the worldmap to a specified new sector.
    * @param string $sector
    */
   void set_sector(const std::string& sector);
   /**
    * @scripting
-   * Changes the current sector of the worldmap to a specified new sector,
-     moving Tux to the specified spawnpoint.
+   * @description Changes the current sector of the worldmap to a specified new sector,
+                  moving Tux to the specified spawnpoint.
    * @param string $sector
    * @param string $spawnpoint
    */
   void spawn(const std::string& sector, const std::string& spawnpoint);
   /**
    * @scripting
-   * Moves Tux to the specified spawnpoint.
+   * @description Moves Tux to the specified spawnpoint.
    * @param string $spawnpoint
    */
   void move_to_spawnpoint(const std::string& spawnpoint);
@@ -143,13 +143,13 @@ public:
 
   /**
    * @scripting
-   * Gets the path to the worldmap file. Useful for saving worldmap-specific data.
+   * @description Gets the path to the worldmap file. Useful for saving worldmap-specific data.
    */
   std::string get_filename() const;
   /**
    * @scripting
-   * Overrides the "Title Screen Level" property for the world with ""filename"".
-     The newly set level will be used for the title screen, after exiting the world.
+   * @description Overrides the "Title Screen Level" property for the world with ""filename"".
+                  The newly set level will be used for the title screen, after exiting the world.
    * @param string $filename
    */
   void set_title_level(const std::string& filename);
