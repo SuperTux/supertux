4aa368f571930c574888d6a4a67c03ce36c221bc
MatusGuy <MatusGuy@users.noreply.github.com>
2024-08-10 09:28:36 +0100

    granito no longer waves when sitting

--- src/badguy/granito.cpp
+++ src/badguy/granito.cpp
@@ -331,13 +331,11 @@ Granito::initialize()
 
   if (m_type == WALK)
   {
-    m_original_state = STATE_WALK;
-    restore_original_state();
+    walk();
   }
   else if (m_type == SIT)
   {
-    m_original_state = STATE_SIT;
-    restore_original_state();
+    sit();
   }
 
   switch (m_type)
