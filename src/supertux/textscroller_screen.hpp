386909f322e9dc136512955038c556503976b97d
MatusGuy <MatusGuy@users.noreply.github.com>
2024-09-08 23:27:18 +0100

    Fix TextScrollerScreen becoming unresponsive when alt-tabbing

--- src/supertux/textscroller_screen.hpp
+++ src/supertux/textscroller_screen.hpp
@@ -38,6 +38,7 @@ public:
 
   virtual void setup() override;
   virtual void draw(Compositor& compositor) override;
+  virtual void leave() override;
   virtual void update(float dt_sec, const Controller& controller) override;
   virtual IntegrationStatus get_status() const override;
 
@@ -50,6 +51,7 @@ private:
 private:
   TextScrollerScreen(const TextScrollerScreen&) = delete;
   TextScrollerScreen& operator=(const TextScrollerScreen&) = delete;
+
 };
 
 #endif
