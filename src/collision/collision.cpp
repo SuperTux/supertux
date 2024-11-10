257c8aa604931f88ebb51f935247d56661be6a92
Iceyshell <135187509+Iceyshell@users.noreply.github.com>
2024-11-10 10:43:03 +0800

    Update collision.cpp again
    This time, the depth detection logic has been modified to reduce the offset value appropriately so that the player can move more smoothly when close to the ceiling
    Also, adjusted the constraints to add additional handling of top collisions to make sure players don't stay in the ceiling area
    Hope this time everything will be fine

--- src/collision/collision.cpp
+++ src/collision/collision.cpp
@@ -127,7 +127,7 @@ bool rectangle_aatriangle(Constraints* constraints, const Rectf& rect,
   std::cout << "Norm: " << normal << " Depth: " << depth << "\n";
 #endif
 
-  Vector outvec = normal * (depth + 0.2f);
+  Vector outvec = normal * (depth + 0.1f);
 
   const float RDELTA = 1.5f;
   if (p1.x < area.get_left() - RDELTA || p1.x > area.get_right() + RDELTA
@@ -146,7 +146,7 @@ bool rectangle_aatriangle(Constraints* constraints, const Rectf& rect,
       constraints->constrain_bottom(rect.get_bottom() + outvec.y);
       constraints->hit.bottom = true;
       hits_rectangle_bottom = true;
-    } else {
+    } else if(outvec.y > 0 && normal.y > 0){
       constraints->constrain_top(rect.get_top() + outvec.y);
       constraints->hit.top = true;
     }
