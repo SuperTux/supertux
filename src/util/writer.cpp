a341c6b42a78266cd23c70f00a62dfb3402f2a98
Vankata453 <78196474+Vankata453@users.noreply.github.com>
2024-12-05 16:20:50 +0200

    Remove unneeded comparison [ci skip]

--- src/util/writer.cpp
+++ src/util/writer.cpp
@@ -258,7 +258,7 @@ Writer::write_compressed(const std::string& name, const std::vector<unsigned int
   unsigned int repeated_value = 0;
   for (size_t i = 0; i < value.size(); ++i)
   {
-    if (repeater > 0 && value[i] == repeated_value)
+    if (repeater && value[i] == repeated_value)
     {
       ++repeater;
     }
