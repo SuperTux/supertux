a7111341a18664726203bc829457ab8ef2e41af5
MatusGuy <MatusGuy@users.noreply.github.com>
2024-12-16 18:44:51 +0000

    Improve git version checking

--- mk/cmake/GetGitRevisionDescription.cmake
+++ mk/cmake/GetGitRevisionDescription.cmake
@@ -19,44 +19,62 @@
 
 find_package(Git QUIET)
 
+# Thanks CMake! This macro sets the same value for the current scope
+# and the parent scope, because apparently only setting it for
+# the parent scope doesn't work.
+macro(parent_set var val)
+  set(${var} ${val})
+  set(${var} ${val} PARENT_SCOPE)
+endmacro()
+
 macro(git_run)
   cmake_parse_arguments(GIT_RUN "" "RESULT;OUTPUT" "COMMAND" ${ARGN})
 
   if(NOT GIT_FOUND)
-    set(${GIT_RUN_OUTPUT} ${GIT_RUN_OUTPUT}-NOTFOUND)
+    parent_set(${GIT_RUN_OUTPUT} ${GIT_RUN_OUTPUT}-NOTFOUND)
+    parent_set(${GIT_RUN_RESULT} 1)
     # Can't use a return command here because... Macro.
   else()
     execute_process(COMMAND ${GIT_EXECUTABLE} ${GIT_RUN_COMMAND}
                     WORKING_DIRECTORY "${PROJECT_SOURCE_DIR}"
-                    RESULT_VARIABLE ${GIT_RUN_RESULT}
-                    OUTPUT_VARIABLE ${GIT_RUN_OUTPUT})
+                    RESULT_VARIABLE _result
+                    OUTPUT_VARIABLE _output)
 
-    if(${${GIT_RUN_RESULT}} EQUAL 0)
-      string(REPLACE "\n" "" ${GIT_RUN_OUTPUT} "${${GIT_RUN_OUTPUT}}")
+    if(_result EQUAL 0)
+      string(REPLACE "\n" "" _output "${_output}")
     else()
-      message(STATUS "\"${GIT_EXECUTABLE} ${GIT_RUN_COMMAND}\" failed with result \"${${GIT_RUN_RESULT}}\".")
-      set(${GIT_RUN_OUTPUT} ${GIT_RUN_OUTPUT}-NOTFOUND)
+      message(STATUS "\"${GIT_EXECUTABLE} ${GIT_RUN_COMMAND}\" failed with result \"${_result}\".")
+      set(_output ${GIT_RUN_OUTPUT}-NOTFOUND)
     endif()
+
+    parent_set(${GIT_RUN_OUTPUT} ${_output})
+    parent_set(${GIT_RUN_RESULT} ${_result})
   endif()
 endmacro()
 
-macro(git_project_version out)
+function(git_project_version out is_release)
   if(NOT GIT_FOUND OR NOT EXISTS "${PROJECT_SOURCE_DIR}/.git")
-    set(${out} ${out}-NOTFOUND)
-  else()
-    # Tag
-    git_run(COMMAND describe --tags --abbrev=0 OUTPUT GIT_TAG RESULT _tag_result)
-
-    # Commit hash
-    git_run(COMMAND rev-parse --short HEAD OUTPUT GIT_HASH RESULT _hash_result)
-
-    # Branch
-    git_run(COMMAND rev-parse --abbrev-ref HEAD OUTPUT GIT_BRANCH RESULT _branch_result)
-
-    if(${_tag_result} EQUAL 128)
-      set(${out} "${GIT_HASH} (${GIT_BRANCH})")
-    else()
-      set(${out} "${GIT_TAG} - ${GIT_HASH} (${GIT_BRANCH})")
-    endif()
+    parent_set(${out} ${out}-NOTFOUND)
+    return()
   endif()
-endmacro()
+
+  # Tag
+  git_run(COMMAND describe --tags --abbrev=0 OUTPUT GIT_TAG RESULT _tag_result)
+
+  if(is_release)
+    parent_set(${out} "${GIT_TAG}")
+    return()
+  endif()
+
+  # Commit hash
+  git_run(COMMAND rev-parse --short HEAD OUTPUT GIT_HASH RESULT _hash_result)
+
+  # Branch
+  git_run(COMMAND rev-parse --abbrev-ref HEAD OUTPUT GIT_BRANCH RESULT _branch_result)
+
+  if(NOT _tag_result EQUAL 0)
+    parent_set(${out} "${GIT_HASH} (${GIT_BRANCH})")
+  else()
+    parent_set(${out} "${GIT_TAG} - ${GIT_HASH} (${GIT_BRANCH})")
+  endif()
+endfunction()
--- mk/cmake/SuperTux/BuildVersion.cmake
+++ mk/cmake/SuperTux/BuildVersion.cmake
@@ -1,11 +1,20 @@
 include(GetGitRevisionDescription)
 
-git_project_version(SUPERTUX_PACKAGE_VERSION)
+git_project_version(SUPERTUX_PACKAGE_VERSION ${IS_SUPERTUX_RELEASE})
 
 if(NOT IS_SUPERTUX_RELEASE AND SUPERTUX_PACKAGE_VERSION)
   string(PREPEND SUPERTUX_PACKAGE_VERSION "dev ")
   set(SUPERTUX_VERSION_STRING "${GIT_BRANCH}-${GIT_HASH}")
+else()
+  set(SUPERTUX_VERSION_STRING "${GIT_TAG}")
 endif()
+
+message(STATUS "Git version checking results:")
+message(STATUS "  GIT_TAG: ${GIT_TAG}")
+message(STATUS "  GIT_HASH: ${GIT_HASH}")
+message(STATUS "  GIT_BRANCH: ${GIT_BRANCH}")
+message(STATUS "  SUPERTUX_PACKAGE_VERSION: ${SUPERTUX_PACKAGE_VERSION}")
+message(STATUS "  SUPERTUX_VERSION_STRING: ${SUPERTUX_VERSION_STRING}")
 
 configure_file(version.h.in ${CMAKE_BINARY_DIR}/version.h)
 
