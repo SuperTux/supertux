SuperTux/supertux

commit 2451a82625ed7bcaa530573c8e6016f66843cf51

Squirrel: Default argument support for exposed C++ functions (#3025)
This PR updates the simplesquirrel submodule to a branch which supports exposing C++ functions (including class constructors) with default arguments.

The `Camera::set_scale()`, `scripting::Level::spawn()`, `scripting::Globals::wait()` and `MovingSprite::set_action()` functions were modified to support default arguments and their respective other functions were marked as deprecated. In case more functions are found suitable for this, this PR would be updated.

tools/scripting_docs_gen/main.cpp
@@ -133,9 +133,6 @@ int main(int argc, char** argv)
     // Formatting
     replace(target_data, "${SRG_NEWPARAGRAPH} ", "\r\n\r\n");
     replace(target_data, "${SRG_TABLENEWPARAGRAPH}", "<br /><br />");
-    replace(target_data, "\"\"", "`");
-    replace(target_data, "NOTE:", "<br /><br />**NOTE:**");
-    replace(target_data, "Note:", "<br /><br />**NOTE:**");
 
     // Write to target file
     write_file(output_dir_path / std::filesystem::path("Scripting" + cl.name + ".md"), target_data);

tools/scripting_docs_gen/writer.cpp
@@ -22,6 +22,14 @@
 
 namespace Writer {
 
+static std::string format_description(std::string desc)
+{
+  replace(desc, "\"\"", "`");
+  replace(desc, "NOTE:", "<br /><br />**NOTE:**");
+  replace(desc, "Note:", "<br /><br />**NOTE:**");
+  return desc;
+}
+
 std::string write_file_notice(const std::string& template_file)
 {
   std::stringstream notice;
@@ -84,7 +92,7 @@ std::string write_constants_table(const std::vector<Constant>& constants)
   {
     // Print out type, name, initializer (if available) and description
     table << "`" << con.type << " " << con.name << (con.initializer.empty() ? "" : " " + con.initializer)
-          << "` | " << con.description << std::endl;
+          << "` | " << format_description(con.description) << std::endl;
   }
 
   return table.str();
@@ -103,7 +111,7 @@ std::string write_variables_table(const std::vector<Variable>& variables)
   for (const Variable& var : variables)
   {
     // Print out type, name and description
-    table << "`" << var.type << " " << var.name << "` | " << var.description << std::endl;
+    table << "`" << var.type << " " << var.name << "` | " << format_description(var.description) << std::endl;
   }
 
   return table.str();
@@ -127,6 +135,8 @@ std::string write_function_table(const std::vector<Function>& functions)
     {
       if (i != 0) table << ", ";
       table << func.parameters[i].type << " " << func.parameters[i].name;
+      if (!func.parameters[i].default_value.empty())
+        table << " = " << func.parameters[i].default_value;
     }
     table << ")`";
 
@@ -136,13 +146,13 @@ std::string write_function_table(const std::vector<Function>& functions)
     {
       table << "**Deprecated!**";
       if (!func.deprecation_msg.empty())
-        table << " " << func.deprecation_msg;
+        table << " " << format_description(func.deprecation_msg);
 
       // Add line breaks only if a description is available
       if (!func.description.empty())
         table << "<br /><br />";
     }
-    table << func.description;
+    table << format_description(func.description);
 
     // Print out descriptions of parameters
     if (std::any_of(func.parameters.begin(), func.parameters.end(), [](const Parameter& param) { return !param.description.empty(); }))
@@ -155,7 +165,7 @@ std::string write_function_table(const std::vector<Function>& functions)
       {
         if (param.description.empty()) continue;
 
-        table << (has_printed_param_desc ? "<br />" : "") << " `" << param.name << "` - " << param.description;
+        table << (has_printed_param_desc ? "<br />" : "") << " `" << param.name << "` - " << format_description(param.description);
         has_printed_param_desc = true;
       }
     }

src/object/camera.cpp
@@ -783,7 +783,7 @@ Camera::set_mode(const std::string& mode)
 }
 
 void
-Camera::ease_scale_anchor(float scale, float time, int anchor, const std::string& ease)
+Camera::set_scale(float scale, float time, int anchor, const std::string& ease)
 {
   ease_scale(scale, time, getEasingByName(EasingMode_from_string(ease)), static_cast<AnchorPoint>(anchor));
 }
@@ -862,12 +862,12 @@ Camera::register_class(ssq::VM& vm)
   cls.addFunc<void, Camera, float, float, float>("scroll_to", &Camera::scroll_to);
   cls.addFunc("get_current_scale", &Camera::get_current_scale);
   cls.addFunc("get_target_scale", &Camera::get_target_scale);
-  cls.addFunc("set_scale", &Camera::set_scale);
-  cls.addFunc("set_scale_anchor", &Camera::set_scale_anchor);
-  cls.addFunc("scale", &Camera::scale);
-  cls.addFunc("scale_anchor", &Camera::scale_anchor);
-  cls.addFunc<void, Camera, float, float, const std::string&>("ease_scale", &Camera::ease_scale);
-  cls.addFunc("ease_scale_anchor", &Camera::ease_scale_anchor);
+  cls.addFunc("set_scale", &Camera::set_scale, ssq::DefaultArguments<float, int, const std::string&>(0.f, AnchorPoint::ANCHOR_MIDDLE, ""));
+  cls.addFunc("set_scale_anchor", &Camera::set_scale_anchor); // Deprecated
+  cls.addFunc("scale", &Camera::scale); // Deprecated
+  cls.addFunc("scale_anchor", &Camera::scale_anchor); // Deprecated
+  cls.addFunc<void, Camera, float, float, const std::string&>("ease_scale", &Camera::ease_scale); // Deprecated
+  cls.addFunc("ease_scale_anchor", &Camera::set_scale); // Deprecated
   cls.addFunc("get_screen_width", &Camera::get_screen_width);
   cls.addFunc("get_screen_height", &Camera::get_screen_height);
   cls.addFunc("get_x", &Camera::get_x);

src/object/camera.hpp
@@ -161,44 +161,55 @@ public:
   inline void scroll_to(float x, float y, float scrolltime) { scroll_to(Vector(x, y), scrolltime); }
   /**
    * @scripting
-   * @description Sets the scale factor.
+   * @description Fades to a specified scale factor and target position anchor in ""time"" seconds with easing (smooth movement).
+                  NOTE: Target position anchor is only applied, if the camera is in "manual" mode.
    * @param float $scale
+   * @param float $time
+   * @param int $anchor Anchor point as represented by the ""ANCHOR_*"" constants.
+                        Optional, default is ""ANCHOR_MIDDLE"" (see ${SRG_REF_AnchorPoints}).
+   * @param string $ease Optional, empty by default.
    */
-  inline void set_scale(float scale) { ease_scale(scale, 0.f, ""); }
+  void set_scale(float scale, float time = 0.f, int anchor = ANCHOR_MIDDLE, const std::string& ease = "");
   /**
    * @scripting
+   * @deprecated Use ""set_scale()"" instead!
    * @description Sets the scale factor and the target position anchor.
                   NOTE: Target position anchor is only applied, if the camera is in "manual" mode.
    * @param float $scale
    * @param int $anchor Anchor point as represented by the ""ANCHOR_*"" constants (see ${SRG_REF_AnchorPoints}).
    */
-  inline void set_scale_anchor(float scale, int anchor) { ease_scale_anchor(scale, 0, anchor, ""); }
+  inline void set_scale_anchor(float scale, int anchor) { set_scale(scale, 0, anchor, ""); }
   /**
    * @scripting
+   * @deprecated Use ""set_scale()"" instead!
    * @description Fades to a specified scale factor in ""time"" seconds.
    * @param float $scale
    * @param float $time
    */
-  inline void scale(float scale, float time) { ease_scale(scale, time, ""); }
+  inline void scale(float scale, float time) { set_scale(scale, time, AnchorPoint::ANCHOR_MIDDLE, ""); }
   /**
    * @scripting
+   * @deprecated Use ""set_scale()"" instead!
    * @description Fades to a specified scale factor and target position anchor in ""time"" seconds.
                   NOTE: Target position anchor is only applied, if the camera is in "manual" mode.
    * @param float $scale
    * @param float $time
    * @param int $anchor Anchor point as represented by the ""ANCHOR_*"" constants (see ${SRG_REF_AnchorPoints}).
    */
-  inline void scale_anchor(float scale, float time, int anchor) { ease_scale_anchor(scale, time, anchor, ""); }
+  inline void scale_anchor(float scale, float time, int anchor) { set_scale(scale, time, anchor, ""); }
   /**
    * @scripting
+   * @deprecated Use ""set_scale()"" instead!
    * @description Fades to a specified scale factor in ""time"" seconds with easing (smooth movement).
    * @param float $scale
    * @param float $time
    * @param string $ease
    */
-  inline void ease_scale(float scale, float time, const std::string& ease) { ease_scale_anchor(scale, time, AnchorPoint::ANCHOR_MIDDLE, ease); }
+  inline void ease_scale(float scale, float time, const std::string& ease) { set_scale(scale, time, AnchorPoint::ANCHOR_MIDDLE, ease); }
+#ifdef DOXYGEN_SCRIPTING
   /**
    * @scripting
+   * @deprecated Use ""set_scale()"" instead!
    * @description Fades to a specified scale factor and target position anchor in ""time"" seconds with easing (smooth movement).
                   NOTE: Target position anchor is only applied, if the camera is in "manual" mode.
    * @param float $scale
@@ -207,6 +218,7 @@ public:
    * @param string $ease
    */
   void ease_scale_anchor(float scale, float time, int anchor, const std::string& ease);
+#endif
   /**
    * @scripting
    * @description Gets the current width of the screen.

src/object/floating_image.cpp
@@ -124,6 +124,7 @@ FloatingImage::register_class(ssq::VM& vm)
 
       return &Sector::get().add<FloatingImage>(spritefile);
     },
+    {},
     false /* Do not free pointer from Squirrel */,
     vm.findClass("GameObject"));
 

src/object/moving_sprite.cpp
@@ -137,13 +137,6 @@ MovingSprite::update_hitbox()
 }
 
 void
-MovingSprite::set_action(const std::string& name)
-{
-  m_sprite->set_action(name);
-  update_hitbox();
-}
-
-void
 MovingSprite::set_action(const std::string& name, int loops)
 {
   m_sprite->set_action(name, loops);
@@ -256,8 +249,8 @@ MovingSprite::register_class(ssq::VM& vm)
   cls.addFunc("set_sprite", &MovingSprite::change_sprite);
   cls.addFunc("get_sprite", &MovingSprite::get_sprite_name);
   cls.addFunc("get_action", &MovingSprite::get_action);
-  cls.addFunc<void, MovingSprite, const std::string&>("set_action", &MovingSprite::set_action);
-  cls.addFunc("set_action_loops", &MovingSprite::set_action_loops);
+  cls.addFunc<void, MovingSprite, const std::string&, int>("set_action", &MovingSprite::set_action, ssq::DefaultArguments<int>(-1));
+  cls.addFunc<void, MovingSprite, const std::string&, int>("set_action_loops", &MovingSprite::set_action); // Deprecated
 
   cls.addVar("sprite", &MovingSprite::get_sprite_name, &MovingSprite::set_sprite);
 }

src/object/moving_sprite.hpp
@@ -100,22 +100,23 @@ public:
   std::string get_action() const;
   /**
    * @scripting
-   * @description Sets the current action of the sprite and resizes the bounding box.
-                  NOTE: Use with care as you can easily get stuck when resizing the bounding box.
-   * @param string $name
-   */
-  void set_action(const std::string& name);
-  /**
-   * @scripting
    * @description Sets the current action of the sprite, as well as the number of times it should loop, and resizes the bounding box.
                   NOTE: Use with care as you can easily get stuck when resizing the bounding box.
    * @param string $name
-   * @param int $loops
+   * @param int $loops Optional, -1 by default (negative value means infinite). Set to -100 to continue looping from the previous action.
    */
-  inline void set_action_loops(const std::string& name, int loops) { set_action(name, loops); }
-
-  /** Sets the action from an action name, as well as the number of times it should loop. */
-  void set_action(const std::string& name, int loops);
+  void set_action(const std::string& name, int loops = -1);
+#ifdef DOXYGEN_SCRIPTING
+  /**
+   * @scripting
+   * @deprecated Use ""set_action()"" instead!
+   * @description Sets the current action of the sprite, as well as the number of times it should loop, and resizes the bounding box.
+                  NOTE: Use with care as you can easily get stuck when resizing the bounding box.
+   * @param string $name
+   * @param int $loops Negative value means infinite. Set to -100 to continue looping from the previous action.
+   */
+  void set_action_loops(const std::string& name, int loops);
+#endif
 
   /** Sets the action from an action name and a particular direction
       in the form of "name-direction", eg. "walk-left".

src/object/text_array_object.cpp
@@ -422,6 +422,7 @@ TextArrayObject::register_class(ssq::VM& vm)
 
       return &Sector::get().add<TextArrayObject>();
     },
+    {},
     false /* Do not free pointer from Squirrel */,
     vm.findClass("GameObject"));
 

commit 9ee7976cb539f766a09b52e6c287f7b4e1a59c1d

Scripting reference: Format both "Summary" and "Instances"
Fixes a formatting issue, introduced with 2451a82625ed7bcaa530573c8e6016f66843cf51.

tools/scripting_docs_gen/main.cpp
@@ -122,8 +122,8 @@ int main(int argc, char** argv)
     std::string target_data = Writer::write_file_notice(page_template_filename) +  page_template;
 
     // Entries
-    replace(target_data, "${SRG_CLASSSUMMARY}", cl.summary, "None.");
-    replace(target_data, "${SRG_CLASSINSTANCES}", cl.instances, "None.");
+    replace(target_data, "${SRG_CLASSSUMMARY}", Writer::format_description(std::move(cl.summary)), "None.");
+    replace(target_data, "${SRG_CLASSINSTANCES}", Writer::format_description(std::move(cl.instances)), "None.");
     replace(target_data, "${SRG_CLASSINHERITANCE}", Writer::write_inheritance_list(classes, cl.base_classes, cl.derived_classes), "None.");
     replace(target_data, "${SRG_CLASSCONSTANTS}", Writer::write_constants_table(cl.constants), "None.");
     replace(target_data, "${SRG_CLASSVARIABLES}", Writer::write_variables_table(cl.variables), "None.");

tools/scripting_docs_gen/writer.cpp
@@ -22,13 +22,14 @@
 
 namespace Writer {
 
-static std::string format_description(std::string desc)
+std::string format_description(std::string desc)
 {
   replace(desc, "\"\"", "`");
   replace(desc, "NOTE:", "<br /><br />**NOTE:**");
   replace(desc, "Note:", "<br /><br />**NOTE:**");
   return desc;
 }
+
 
 std::string write_file_notice(const std::string& template_file)
 {

tools/scripting_docs_gen/writer.hpp
@@ -24,6 +24,8 @@
 
 namespace Writer
 {
+  std::string format_description(std::string desc);
+
   std::string write_file_notice(const std::string& template_file);
 
   std::string write_inheritance_list(const std::vector<Class>& classes,