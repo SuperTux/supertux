2dd5a35793d9ca2248d55046fe04a19485bfda02
Tobias Markus <tobbi.bugs@googlemail.com>
2025-01-11 16:14:22 +0100

    Add visual indicator of trigger direction to editor

--- src/trigger/scripttrigger.cpp
+++ src/trigger/scripttrigger.cpp
@@ -98,8 +98,55 @@ void
 ScriptTrigger::draw(DrawingContext& context)
 {
   if (Editor::is_active() || g_debug.show_collision_rects)
-    context.color().draw_filled_rect(m_col.m_bbox, Color(1.0f, 0.0f, 1.0f, 0.6f),
-                             0.0f, LAYER_OBJECTS);
+  {
+    const auto& bbox = m_col.m_bbox;
+    const auto& color = Color(1.0f, 0.0f, 1.0f, 0.6f);
+    const float transparency = 0.0f;
+    const int layer = LAYER_OBJECTS;
+
+    context.color().draw_filled_rect(bbox, color, transparency, layer);
+
+    if (Editor::is_active() && m_trigger_direction != Direction::NONE)
+    {
+      const float indicator_width = 2.0f;
+      const float indicator_length = 50.0f;
+      std::vector<Rectf> indicators;
+
+      Rectf left_indicator = Rectf(bbox.get_left() - indicator_length,
+                                   bbox.get_middle().y - indicator_width, 
+                                   bbox.get_left(),
+                                   bbox.get_middle().y + indicator_width);
+      
+      Rectf right_indicator = Rectf(bbox.get_right(),
+                                    bbox.get_middle().y - indicator_width,
+                                    bbox.get_right() + indicator_length,
+                                    bbox.get_middle().y + indicator_width);
+
+      Rectf up_indicator = Rectf(bbox.get_middle().x - indicator_width,
+                                 bbox.get_top() - indicator_length, 
+                                 bbox.get_middle().x + indicator_width,
+                                 bbox.get_top());
+
+      Rectf down_indicator = Rectf(bbox.get_middle().x - indicator_width,
+                                   bbox.get_bottom(), 
+                                   bbox.get_middle().x + indicator_width,
+                                   bbox.get_bottom() + indicator_length);
+
+      if(m_trigger_direction == Direction::LEFT  || m_trigger_direction == Direction::AUTO)
+        indicators.push_back(left_indicator);
+      if(m_trigger_direction == Direction::RIGHT || m_trigger_direction == Direction::AUTO)
+        indicators.push_back(right_indicator);
+      if(m_trigger_direction == Direction::UP    || m_trigger_direction == Direction::AUTO)
+        indicators.push_back(up_indicator);
+      if(m_trigger_direction == Direction::DOWN  || m_trigger_direction == Direction::AUTO)
+        indicators.push_back(down_indicator);
+      
+      for(const auto& dir_indicator : indicators)
+      {
+        context.color().draw_filled_rect(dir_indicator, color, transparency, layer);
+      }
+    }  
+  }
 }
 
 /* EOF */
