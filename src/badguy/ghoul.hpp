cfd24dd2c5c7a002ab07f27bbe4c10e0568c8497
Hypernoot <teratux.mail@gmail.com>
2025-03-31 20:29:41 +0200

    Fix rebase errors

--- src/badguy/ghoul.hpp
+++ src/badguy/ghoul.hpp
@@ -26,7 +26,7 @@ public:
   static std::string display_name() { return _("Ghoul"); }
   std::string get_class_name() const override { return class_name(); }
   std::string get_display_name() const override { return display_name(); }
-  virtual GameObjectClasses get_class_types() const override { return BadGuy::get_class_types().add(typeid(PathObject)).add(typeid(Ghoul)); }
+  virtual GameObjectClasses get_class_types() const override { return BadGuy::get_class_types().add(typeid(Ghoul)); }
   virtual bool is_snipable() const override { return true; }
   virtual bool is_flammable() const override { return false; }
 
