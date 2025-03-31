9a418764c92bbe9ad4fccba066b2049697e2e14e
Jacob Burroughs <maths22@gmail.com>
2025-03-31 15:18:34 -0500

    Update deploy.sh

--- .ci_scripts/deploy.sh
+++ .ci_scripts/deploy.sh
@@ -1,13 +1,11 @@
 #!/usr/bin/env bash
 
 shopt -s nullglob
-# msys bash tries to be "helpful" and convert URL into a windows-based absolute path without this
-MSYS2_ARG_CONV_EXCL=URL
 
 for file in upload/SuperTux*; do
     file_base=$(basename $file)
     echo "Uploading $file_base";
-    export URL="/${PREFIX}/$file_base"
+    export URL="${PREFIX}/$file_base"
     export SIZE=$(($(wc -c < "$file")))
     if [ $IS_WINDOWS = true ] ; then
         export SHASUM=$(powershell -command "Get-FileHash \"$file\" -Algorithm SHA256 | Select-Object -ExpandProperty Hash")
@@ -16,7 +14,7 @@ for file in upload/SuperTux*; do
     fi
     echo "Checksum: $SHASUM";
     echo "Branch: $BRANCH_NAME";
-    jq -n '{"url": env["URL"], "shasum": env["SHASUM"], "size": env["SIZE"] | tonumber, "branch": env["BRANCH_NAME"] }' | \
+    jq -n '{"url": ("/" + env["URL"]), "shasum": env["SHASUM"], "size": env["SIZE"] | tonumber, "branch": env["BRANCH_NAME"] }' | \
         curl -X POST \
             -H "x-api-key: $DOWNLOAD_APIKEY" \
             -H "Content-Type: application/json" \
