03704383433b1f1448bcb1237e8a7264741e0f99
Jacob Burroughs <maths22@gmail.com>
2025-04-03 08:12:49 -0500

    Update deploy.sh for aws-based service (#3229)
    * Update deploy.sh for aws-based service
    
    * Update deploy.sh
    
    * Update deploy.sh
    
    * Update deploy.sh
    
    * Update deploy.sh

--- .ci_scripts/deploy.sh
+++ .ci_scripts/deploy.sh
@@ -5,19 +5,19 @@ shopt -s nullglob
 for file in upload/SuperTux*; do
     file_base=$(basename $file)
     echo "Uploading $file_base";
-    url="https://supertux-ci-downloads.s3-us-west-2.amazonaws.com/${PREFIX}/$file_base"
-    size=$(($(wc -c < "$file")))
+    export URL="${PREFIX}/$file_base"
+    export SIZE=$(($(wc -c < "$file")))
     if [ $IS_WINDOWS = true ] ; then
-        shasum=$(powershell -command "Get-FileHash \"$file\" -Algorithm SHA256 | Select-Object -ExpandProperty Hash")
+        export SHASUM=$(powershell -command "Get-FileHash \"$file\" -Algorithm SHA256 | Select-Object -ExpandProperty Hash")
     else
-        shasum=$(shasum -a 256 "$file" | cut -d " " -f 1)
+        export SHASUM=$(shasum -a 256 "$file" | cut -d " " -f 1)
     fi
-    echo "Checksum: $shasum";
+    echo "Checksum: $SHASUM";
     echo "Branch: $BRANCH_NAME";
-    curl --data "apikey=$DOWNLOAD_APIKEY" \
-         --data "url=$url" \
-         --data "size=$size" \
-         --data "branch=$BRANCH_NAME" \
-         --data "shasum=$shasum" \
-         -L -s https://download.supertux.org/submit.php
+    jq -n '{"url": ("/" + env["URL"]), "shasum": env["SHASUM"], "size": env["SIZE"] | tonumber, "branch": env["BRANCH_NAME"] }' | \
+        curl -X POST \
+            -H "x-api-key: $DOWNLOAD_APIKEY" \
+            -H "Content-Type: application/json" \
+            --data-binary @- \
+            -s https://download.supertux.org/api/download
 done
