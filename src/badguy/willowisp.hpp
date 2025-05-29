4b3c6e005c6d641aec7de18fc5b12422efdbda65
bruhmoent <kmega757@gmail.com>
2025-05-29 16:56:22 +0200

    Add support for paths for `WillOWisps` in the editor

--- src/badguy/willowisp.hpp
+++ src/badguy/willowisp.hpp
@@ -69,6 +69,7 @@ public:
   static std::string display_name() { return _("Will o' Wisp"); }
   virtual std::string get_display_name() const override { return display_name(); }
   virtual GameObjectClasses get_class_types() const override { return BadGuy::get_class_types().add(typeid(PathObject)).add(typeid(WillOWisp)); }
+  virtual void editor_update() override;
 
   virtual ObjectSettings get_settings() override;
   virtual void move_to(const Vector& pos) override;
