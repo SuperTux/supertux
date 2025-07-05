7aabf39aae024fbebe31ecc4b81c3f035d839d51
Tobias Markus <tobbi.bugs@googlemail.com>
2025-07-05 04:47:33 +0200

    WIP: Properties for clicked layer items

--- src/editor/layers_widget.cpp
+++ src/editor/layers_widget.cpp
@@ -218,10 +218,12 @@ EditorLayersWidget::on_mouse_button_down(const SDL_MouseButtonEvent& button)
           if (tilemap) {
             set_selected_tilemap(tilemap);
             m_editor.edit_path(tilemap->get_path_gameobject(), tilemap);
+            m_editor.update_properties_panel(tilemap);
           } else {
             auto cam = dynamic_cast<Camera*>(m_layer_icons[m_hovered_layer]->get_layer());
             if (cam) {
               m_editor.edit_path(cam->get_path_gameobject(), cam);
+              m_editor.update_properties_panel(cam);
             }
           }
         }
@@ -237,6 +239,7 @@ EditorLayersWidget::on_mouse_button_down(const SDL_MouseButtonEvent& button)
       auto om = std::make_unique<ObjectMenu>(m_layer_icons[m_hovered_layer]->get_layer());
       m_editor.m_deactivate_request = true;
       MenuManager::instance().push_menu(std::move(om));
+      m_editor.update_properties_panel(m_layer_icons[m_hovered_layer]->get_layer());
       return true;
     } else {
       return false;
