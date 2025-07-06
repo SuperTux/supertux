ca203aa4baf30d6d996d90f2495aa54acd4f5d50
johannes-el <Johannes.Elsing@gmx.de>
2025-07-06 14:29:44 +0200

    Remove EOF newline in discord.hpp

--- src/sdk/discord.hpp
+++ src/sdk/discord.hpp
@@ -14,8 +14,7 @@
 //  You should have received a copy of the GNU General Public License
 //  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-#ifndef HEADER_SUPERTUX_SDK_DISCORD_HPP
-#define HEADER_SUPERTUX_SDK_DISCORD_HPP
+#pragma once
 
 #include "config.h"
 
