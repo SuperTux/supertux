6b3e494f211f492d657cdbffea0d086b262cc0ae
Tobias Markus <tobbi.bugs@googlemail.com>
2025-09-01 12:50:56 +0200

    MovingSprite: Check if action exists before setting it

--- src/object/moving_sprite.cpp
+++ src/object/moving_sprite.cpp
@@ -218,7 +218,10 @@ MovingSprite::after_editor_set()
   {
     change_sprite(get_default_sprite_name());
   }
-  m_sprite->set_action(current_action);
+  if (m_sprite->has_action(current_action))
+  {
+    m_sprite->set_action(current_action);
+  }
 
   update_hitbox();
 }
