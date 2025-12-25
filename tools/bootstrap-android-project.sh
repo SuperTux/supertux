5a101ac0168f00705b2e50cfec98e1f7069a92aa
Swagtoy <me@ow.swag.toys>
2025-12-25 04:00:34 -0500

    Fix CI take 3

--- tools/bootstrap-android-project.sh
+++ tools/bootstrap-android-project.sh
@@ -20,6 +20,7 @@ fi
 git clone https://github.com/libsdl-org/SDL.git -b release-${VER} SDL-work --depth 1
 
 _snack "settings.gradle"
+_snack "gradlew"
 _snack "gradle/wrapper/gradle-wrapper.jar"
 _snack "app/proguard-rules.pro"
 cp -r "$IN/app/src/main/java" "$out/app/src/main"
