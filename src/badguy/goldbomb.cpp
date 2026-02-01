dc5eaaebf791a0c42f45f2785c9f377d4165e048
Frostwithasideofsalt <60326522+Frostwithasideofsalt@users.noreply.github.com>
2026-01-31 17:25:39 -0800

    john codestyle [ci skip]

--- src/badguy/goldbomb.cpp
+++ src/badguy/goldbomb.cpp
@@ -198,7 +198,8 @@ GoldBomb::explode()
   const bool was_glinting = m_is_glinting;
   MrBomb::explode();
 
-  if (was_glinting){
+  if (was_glinting)
+  {
     Sector::get().add<CoinExplode>(get_pos(), was_glinting);
   }
 }
