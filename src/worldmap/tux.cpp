4fc737c9fa95792098d1a92c71ba3356aa94f523
MatusGuy <MatusGuy@users.noreply.github.com>
2026-06-25 18:09:30 +0100

    fix anomaly in walking code

--- src/worldmap/tux.cpp
+++ src/worldmap/tux.cpp
@@ -304,20 +304,19 @@ Tux::try_continue_walking(float dt_sec)
       (teleporter) ||
       m_ghost_mode)
   {
-      if (special_tile && !special_tile->get_map_message().empty() && !special_tile->is_passive_message())
-      {
-        m_worldmap->set_passive_message("", 0.0f);
-      }
+    if (special_tile && !special_tile->get_map_message().empty() && !special_tile->is_passive_message())
+    {
+      m_worldmap->set_passive_message("", 0.0f);
+    }
 
-      if (worldmap_sector->at_object<LevelTile>() != nullptr)
-      {
-        m_last_level_tile_pos = m_tile_pos;
-        m_last_level_back_direction = m_back_direction;
-      }
+    if (worldmap_sector->at_object<LevelTile>() != nullptr)
+    {
+      m_last_level_tile_pos = m_tile_pos;
+      m_last_level_back_direction = m_back_direction;
+    }
 
-      m_last_stable_tile_pos = m_ghost_mode ? m_last_level_tile_pos : m_tile_pos;
-      m_last_stable_back_direction = m_ghost_mode ? m_last_level_back_direction : m_back_direction;
-  }
+    m_last_stable_tile_pos = m_ghost_mode ? m_last_level_tile_pos : m_tile_pos;
+    m_last_stable_back_direction = m_ghost_mode ? m_last_level_back_direction : m_back_direction;
 
     stop();
     return;
