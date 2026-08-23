683ce4c1b4e02c7b3703b1f66c3c9654977b6200
Tobias Markus <tobbi.bugs@googlemail.com>
2026-08-23 21:03:53 +0200

    Editor refactoring (WIP): Move tile conversion into new class

---
+++ src/editor/editor_tile_converter.cpp
@@ -0,0 +1,116 @@
+//  SuperTux
+//  Copyright (C) 2026 Tobias Markus <tobbi.bugs@googlemail.com>
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+#include "editor/editor.hpp"
+#include "editor/editor_tile_converter.hpp"
+#include "object/camera.hpp"
+#include "object/tilemap.hpp"
+#include "physfs/ifile_stream.hpp"
+#include "supertux/level.hpp"
+
+void
+EditorTileConverter::check_deprecated_tiles(bool focus)
+{
+  auto editor = Editor::current();
+  auto layers_widget = editor->get_layers_widget();
+
+  // Check for any deprecated tiles, used throughout the entire level
+  m_has_deprecated_tiles = false;
+  for (const auto& sector : editor->get_level()->get_sectors())
+  {
+    for (auto& tilemap : sector->get_objects_by_type<TileMap>())
+    {
+      int pos = -1;
+      for (const uint32_t& tile_id : tilemap.get_tiles())
+      {
+        pos++;
+        if (editor->get_tileset()->get(tile_id).is_deprecated())
+        {
+          // Focus on deprecated tile
+          if (focus)
+          {
+            editor->set_sector(sector.get());
+            layers_widget->set_selected_tilemap(&tilemap);
+
+            const int width = tilemap.get_width();
+            sector->get_camera().set_translation_centered(Vector(pos % width, pos / width) * 32.f);
+            editor->keep_camera_in_bounds();
+          }
+
+          m_has_deprecated_tiles = true;
+          return;
+        }
+      }
+    }
+  }
+}
+
+void
+EditorTileConverter::convert_tiles_by_file(const std::string& file)
+{
+  auto editor = Editor::current();
+  auto level = editor->get_level();
+
+  std::unordered_map<int, int> tiles;
+
+  try
+  {
+    IFileStream in(file);
+    if (!in.good())
+      throw std::runtime_error("Error opening file stream!");
+
+    int a, b;
+    std::string delimiter;
+    while (in >> a >> delimiter >> b)
+    {
+      if (delimiter != "->")
+        throw std::runtime_error("Expected '->' delimiter!");
+
+      tiles[a] = b;
+    }
+  }
+  catch (std::exception& err)
+  {
+    log_warning << "Couldn't parse conversion file '" << file << "': " << err.what() << std::endl;
+    return;
+  }
+
+  for (const auto& sector : level->get_sectors())
+  {
+    for (auto& tilemap : sector->get_objects_by_type<TileMap>())
+    {
+      tilemap.save_state();
+      // Can't use change_all(), if there's like `1 -> 2`and then
+      // `2 -> 3`, it'll do a double replacement
+      for (int x = 0; x < tilemap.get_width(); x++)
+      {
+        for (int y = 0; y < tilemap.get_height(); y++)
+        {
+          auto tile = tilemap.get_tile_id(x, y);
+          try
+          {
+            tilemap.change(x, y, tiles.at(tile));
+          }
+          catch (std::out_of_range&)
+          {
+            // Expected for tiles that don't need to be replaced
+          }
+        }
+      }
+      tilemap.check_state();
+    }
+  }
+}
