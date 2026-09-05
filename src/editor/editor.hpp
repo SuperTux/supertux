160c32770f1fdb25899220ae57eedb0130c698c1
Tobias Markus <tobbi.bugs@googlemail.com>
2026-09-05 16:27:35 +0200

    Remove `quit_request`

--- src/editor/editor.hpp
+++ src/editor/editor.hpp
@@ -157,7 +157,6 @@ public:
   void set_undo_disabled(bool state);
   void set_redo_disabled(bool state);
 
-
   /**
    * Deactivates the editor so that it doesn't receive
    * any keyboard or mouse events (e.g. when a menu is shown)
@@ -176,13 +175,17 @@ public:
    */
   void reactivate_after_menu_close();
 
+  /**
+   * Exits the editor
+   */
+  void quit();
+
   inline void on_exit(exit_cb_t exit_cb) { m_on_exit_cb = exit_cb; }
 
 private:
   void set_sector(Sector* sector);
   void reload_level();
   void reset_level();
-  void quit_editor();
   /**
    * @param filename    If non-empty, save to this file instead.
    * @param switch_file If true, the level editor will bind itself to the new
@@ -196,7 +199,6 @@ private:
   void add_control(const std::string& name, std::unique_ptr<InterfaceControl> new_control, const std::string& description = "");
 
 public:
-  bool m_quit_request;
   bool m_newlevel_request;
   bool m_reload_request;
   bool m_save_request;
