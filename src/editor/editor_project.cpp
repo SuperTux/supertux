283ea15f888d5a6ab003c4735c5eb8bfc01832f7
Tobias Markus <tobbi.bugs@googlemail.com>
2026-09-06 10:22:20 +0200

    Potential fix for crash when saving level

--- src/editor/editor_project.cpp
+++ src/editor/editor_project.cpp
@@ -195,7 +195,7 @@ EditorProject::save_level(const std::string& filename, bool switch_file,
     sector->on_editor_save();
   }
 
-  m_level->save(m_world ? FileSystem::join(m_world->get_basedir(), file) : file);
+  m_level->save(FileSystem::join(get_level_directory(), file));
   m_time_since_last_save = 0.f;
   remove_autosave_file();
 
