f1f90380054b5af6f73c5684eba1a5bbd51b796f
Tobias Markus <tobbi.bugs@googlemail.com>
2026-09-16 11:13:42 +0200

    Switch CI to MacOS 15 runner

--- .ci_scripts/package.sh
+++ .ci_scripts/package.sh
@@ -2,7 +2,7 @@
 
 shopt -s nullglob
 
-if ([ "$OS_NAME" = "macos-10.15" ] || [ "$OS_NAME" = "macos-14" ]) && [ "$PACKAGE" = "ON" ]; then
+if ([ "$OS_NAME" = "macos-10.15" ] || [ "$OS_NAME" = "macos-15" ]) && [ "$PACKAGE" = "ON" ]; then
     sudo chmod -R +w /usr/local/Cellar
     cpack -G Bundle;
 fi
